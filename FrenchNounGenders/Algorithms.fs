49d3bfeb2a17994a11c1ba391cc6056ee6de7ff2
Rob Keim <robkeim@gmail.com>
2016-07-08 18:33:40 +0200

    Add simplified list of endings algorithm

--- FrenchNounGenders/Algorithms.fs
+++ FrenchNounGenders/Algorithms.fs
@@ -1,5 +1,6 @@
 ﻿module Algorithms
 
+open System.Text.RegularExpressions
 open Types
 
 let alwaysReturnOneGender word gender =
@@ -12,3 +13,26 @@ let alwaysReturnMasculine word =
 // Always return feminine
 let alwaysReturnFeminine word =
     alwaysReturnOneGender word Feminine
+
+let (|RegexMatch|_|) pattern input =
+   let m = Regex.Match(input, "^" + pattern + "$")
+   match Regex.Match(input, "^" + pattern + "$").Success with
+   | true -> Some true
+   | _ -> None
+
+// Rules for only feminine words from "A simplified list of endings" on this site:
+// https://frenchtogether.com/french-nouns-gender/
+// Feminine noun endings
+// - The majority of words that end in -e or -ion.
+// - Except words ending in -age, -ege, -é, or -isme (these endings often indicate masculine words).
+// Masculine noun endings
+// - Most words with other endings are masculine.
+let simplifiedListOfEndings word = 
+    match word with
+    | RegexMatch ".*age" _ -> { Word=word; Gender=Masculine }
+    | RegexMatch ".*ege" _ -> { Word=word; Gender=Masculine }
+    | RegexMatch ".*é" _ -> { Word=word; Gender=Masculine }
+    | RegexMatch ".*isme" _ -> { Word=word; Gender=Masculine }
+    | RegexMatch ".*e" _ -> { Word=word; Gender=Feminine }
+    | RegexMatch ".*ion" _ -> { Word=word; Gender=Feminine }
+    | _ -> { Word=word; Gender=Masculine }
--- FrenchNounGenders/Program.fs
+++ FrenchNounGenders/Program.fs
@@ -6,6 +6,7 @@ open Calculations
 let main argv =
     runAlgorithm alwaysReturnMasculine "Always masculine"
     runAlgorithm alwaysReturnFeminine "Always feminine"
+    runAlgorithm simplifiedListOfEndings "Simplified list of endings"
 
     printfn "\ndone!"
     ignore (Console.ReadLine())
