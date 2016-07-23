8f80ceb7695f8fa58cac82c829a225806370ac22
Rob Keim <robkeim@gmail.com>
2016-07-23 17:27:24 +0200

    Add project skeleton

---
+++ USMortalityDataAnalysis/Program.fs
@@ -0,0 +1,10 @@
+﻿// This data was originally download from here:
+// https://www.kaggle.com/cdc/mortality
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "\ndone!"
+    ignore (Console.ReadLine())
+    0 // return an integer exit code
