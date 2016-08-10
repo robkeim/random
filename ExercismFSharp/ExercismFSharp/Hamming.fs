5d0608fd992c0967851fefa881c22ae4e225c5e9
Rob Keim <robkeim@gmail.com>
2016-08-11 00:33:43 +0200

    [ExercismFSharp] Add Hamming solution

---
+++ ExercismFSharp/ExercismFSharp/Hamming.fs
@@ -0,0 +1,8 @@
+﻿module Hamming
+
+let compute strand1 strand2 =
+    Seq.map2
+        (fun s1 s2 -> s1 = s2)
+        strand1 strand2
+    |> Seq.filter (fun value -> value = false)
+    |> Seq.length
