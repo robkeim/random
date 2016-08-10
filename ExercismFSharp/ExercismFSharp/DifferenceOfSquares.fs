c78dfafd5d42c7e6727f6a9ea4670a20bd88ab59
Rob Keim <robkeim@gmail.com>
2016-08-10 16:50:46 +0200

    [ExercismFSharp] Add DifferenceOfSquares solution

---
+++ ExercismFSharp/ExercismFSharp/DifferenceOfSquares.fs
@@ -0,0 +1,13 @@
+﻿module DifferenceOfSquares
+
+let squareOfSums (num : int) =
+    let sum = [1..num] |> List.sum
+    sum * sum
+
+let sumOfSquares (num : int) =
+    [1..num]
+    |> List.map(fun n -> n * n)
+    |> List.sum
+
+let difference (num : int) =
+    (squareOfSums num) - (sumOfSquares num)
