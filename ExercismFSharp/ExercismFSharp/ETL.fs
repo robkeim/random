20f8c2b78fac57119fdacde3c3bccfc632f3b315
Rob Keim <robkeim@gmail.com>
2016-08-17 00:29:15 +0200

    [ExercismFSharp] Add ETL solution

---
+++ ExercismFSharp/ExercismFSharp/ETL.fs
@@ -0,0 +1,13 @@
+﻿module ETL
+
+let transform input =
+    let mapStringList key values =
+        Seq.map
+            (fun (value : string) -> (value.ToLower(), key))
+            values
+
+    input
+    |> Map.toSeq
+    |> Seq.collect (fun (k, v) -> mapStringList k v)
+    |> Seq.sort
+    |> Map.ofSeq
