04ffd19bdccfd12da03dd173b2051f331bf33329
Rob Keim <robkeim@gmail.com>
2016-08-19 19:28:04 +0200

    [ExercismFSharp] Add RNATranscription solution

---
+++ ExercismFSharp/ExercismFSharp/RnaTranscription.fs
@@ -0,0 +1,13 @@
+﻿module RNATranscription
+
+open System
+
+let toRna dna =
+    dna
+    |> Seq.map (fun c ->
+        match c with
+        | 'G' -> 'C'
+        | 'C' -> 'G'
+        | 'T' -> 'A'
+        | 'A' -> 'U'
+        | _ -> raise (ArgumentException "Invalid DNA character"))
