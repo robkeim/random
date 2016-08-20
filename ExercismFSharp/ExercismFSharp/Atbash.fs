6f03f85b723f315f07e7a41609513e43f62d954f
Rob Keim <robkeim@gmail.com>
2016-08-20 03:29:27 +0200

    [ExercismFSharp] Add Atbash solution

---
+++ ExercismFSharp/ExercismFSharp/Atbash.fs
@@ -0,0 +1,40 @@
+﻿module Atbash
+
+open System
+
+let encode phrase =
+    phrase
+    |> Seq.choose (fun c ->
+        match Char.ToLowerInvariant c with
+        | 'a' -> Some 'z'
+        | 'b' -> Some 'y'
+        | 'c' -> Some 'x'
+        | 'd' -> Some 'w'
+        | 'e' -> Some 'v'
+        | 'f' -> Some 'u'
+        | 'g' -> Some 't'
+        | 'h' -> Some 's'
+        | 'i' -> Some 'r'
+        | 'j' -> Some 'q'
+        | 'k' -> Some 'p'
+        | 'l' -> Some 'o'
+        | 'm' -> Some 'n'
+        | 'n' -> Some 'm'
+        | 'o' -> Some 'l'
+        | 'p' -> Some 'k'
+        | 'q' -> Some 'j'
+        | 'r' -> Some 'i'
+        | 's' -> Some 'h'
+        | 't' -> Some 'g'
+        | 'u' -> Some 'f'
+        | 'v' -> Some 'e'
+        | 'w' -> Some 'd'
+        | 'x' -> Some 'c'
+        | 'y' -> Some 'b'
+        | 'z' -> Some 'a'
+        | _ when c >= '0' && c <= '9' -> Some c
+        | _ -> None
+        )
+    |> Seq.chunkBySize 5
+    |> Seq.map System.String
+    |> Seq.reduce (sprintf "%s %s")
