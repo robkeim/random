a8f6083831634b57d9f08da2bbab256177516697
Rob Keim <robkeim@gmail.com>
2016-08-21 01:46:34 +0200

    [ExercismFSharp] Use caching in NthPrime solution

--- ExercismFSharp/ExercismFSharp/NthPrime.fs
+++ ExercismFSharp/ExercismFSharp/NthPrime.fs
@@ -2,17 +2,21 @@
 
 open System
 
-let nthPrime n =
-    let isPrime n =
-        match n with
-        | 2 -> true
-        | n when n % 2 = 0 -> false
-        | _ ->
-            let max = int (Math.Ceiling (Math.Sqrt (float n)))
-            [3 .. 1 .. max]
-            |> Seq.forall (fun value -> not (n % value = 0))
-    
+let private isPrime n =
+    match n with
+    | 2 -> true
+    | n when n % 2 = 0 -> false
+    | _ ->
+        let max = int (Math.Ceiling (Math.Sqrt (float n)))
+        [3 .. 1 .. max]
+        |> Seq.forall (fun value -> not (n % value = 0))
+
+let private primesSeq =
     Seq.initInfinite (fun v -> v + 1)
     |> Seq.filter isPrime
+    |> Seq.cache
+
+let nthPrime n =
+    primesSeq
     |> Seq.skip n
     |> Seq.head
