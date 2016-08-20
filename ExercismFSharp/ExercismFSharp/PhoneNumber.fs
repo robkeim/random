fc9813dfd8aa9ad76f6a2e9030cac474a5f5431b
Rob Keim <robkeim@gmail.com>
2016-08-21 01:21:19 +0200

    [ExercismFSharp] Add PhoneNumber solution

---
+++ ExercismFSharp/ExercismFSharp/PhoneNumber.fs
@@ -0,0 +1,20 @@
+﻿module PhoneNumber
+
+open System
+
+let parsePhoneNumber input =
+    let phoneNumber =
+        input
+        |> Seq.filter Char.IsDigit
+        |> Seq.toList
+
+    let length = phoneNumber |> List.length
+
+    let toString phoneNumber =
+        phoneNumber
+        |> List.fold (sprintf "%s%c") ""
+
+    match phoneNumber with
+    | _ when length = 10 -> Some (phoneNumber |> toString)
+    | '1'::pn when length = 11 -> Some (pn |> toString)
+    | _ -> None
