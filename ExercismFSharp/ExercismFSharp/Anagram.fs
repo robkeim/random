4a6bbb12bbcae2741fe835648cbc22d44e1e6c8d
Rob Keim <robkeim@gmail.com>
2016-08-20 02:42:08 +0200

    [ExercismFSharp] Add Anagram solution

---
+++ ExercismFSharp/ExercismFSharp/Anagram.fs
@@ -0,0 +1,20 @@
+﻿module Anagram
+
+open System
+
+let anagrams (candidates : string list) word =
+    let toLower (word : string) =
+        word.ToLowerInvariant()
+
+    let order word =
+        word
+        |> toLower
+        |> Seq.sort
+        |> Seq.toArray
+        |> String
+
+    let orderedWord = word |> order
+
+    candidates
+    |> List.filter (fun w ->
+        order w = orderedWord && not (toLower w = toLower word))
