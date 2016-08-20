cf241f8fc6db189366e458daee6f32f59dc2e8be
Rob Keim <robkeim@gmail.com>
2016-08-21 01:51:28 +0200

    [ExercismFSharp] Add PerfectNumbers solution

---
+++ ExercismFSharp/ExercismFSharp/PerfectNumbers.fs
@@ -0,0 +1,22 @@
+﻿module PerfectNumbers
+
+open System
+
+type NumberType =
+    | Perfect
+    | Abundant
+    | Deficient
+
+let classify num =
+    let factorSum =
+        [1 .. num - 1]
+        |> Seq.filter (fun n -> num % n = 0)
+        |> Seq.sum
+
+    let diff = factorSum - num
+
+    match diff with
+    | _ when diff < 0 -> NumberType.Deficient
+    | 0 -> NumberType.Perfect
+    | _ when diff > 0 -> NumberType.Abundant
+    | _ -> raise (Exception "unreachable code")
