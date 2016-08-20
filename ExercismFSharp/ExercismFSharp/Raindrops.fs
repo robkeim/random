9082d2208f5d4a65a334be42179cc52e07478e36
Rob Keim <robkeim@gmail.com>
2016-08-20 03:47:19 +0200

    [ExercismFSharp] Add Raindrop solution

---
+++ ExercismFSharp/ExercismFSharp/Raindrops.fs
@@ -0,0 +1,19 @@
+﻿module Raindrops
+
+// This problem seems to be a slightly more complex version of FizzBuzz
+// https://en.wikipedia.org/wiki/Fizz_buzz
+
+// This probably isn't the easiest to read solution, but it's a fun use of pattern matching :)
+let convert num =
+    match num % 3, num % 5, num % 7 with
+    | 0, 0, 0 -> "PlingPlangPlong"
+
+    | 0, 0, _ -> "PlingPlang"
+    | 0, _, 0 -> "PlingPlong"
+    | _, 0, 0 -> "PlangPlong"
+    
+    | 0, _, _ -> "Pling"
+    | _, 0, _ -> "Plang"
+    | _, _, 0 -> "Plong"
+
+    | _, _, _ -> num |> sprintf "%A"
