255e0ed54ae6f9cdd83143a4a6ba29d7b4ce97bb
Rob Keim <robkeim@gmail.com>
2016-08-20 10:49:15 +0200

    [ExercismFSharp] Add NucleoTide solution

---
+++ ExercismFSharp/ExercismFSharp/NucleoTideCount.fs
@@ -0,0 +1,16 @@
+﻿module NucleoTideCount
+
+open System
+
+let count nucleotide strand =
+    match nucleotide with
+    | 'A' | 'T' | 'C' | 'G' -> 
+        strand
+        |> Seq.filter (fun c -> c = nucleotide)
+        |> Seq.length
+    | _ -> raise (Exception "unknown nucleotide type")
+
+let nucleotideCounts strand =
+    [ 'A'; 'T'; 'C'; 'G']
+    |> Seq.map (fun c -> (c, count c strand))
+    |> Map.ofSeq
