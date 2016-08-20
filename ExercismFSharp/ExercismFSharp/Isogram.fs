7888c7fdde85e1fead8017cf62c508b94cc880cd
Rob Keim <robkeim@gmail.com>
2016-08-21 01:28:37 +0200

    [ExercismFSharp] Add Isogram solution

---
+++ ExercismFSharp/ExercismFSharp/Isogram.fs
@@ -0,0 +1,11 @@
+﻿module Isogram
+
+open System
+
+let isogram phrase =
+    let normalizedPhrase =
+        phrase
+        |> Seq.filter Char.IsLetter
+        |> Seq.map Char.ToLowerInvariant
+
+    normalizedPhrase |> Seq.length = (normalizedPhrase |> Seq.distinct |> Seq.length)
