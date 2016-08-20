db17f6c911206cb92adc753b824749d6b18b45ef
Rob Keim <robkeim@gmail.com>
2016-08-20 22:08:19 +0200

    [ExercismFSharp] Add Palindrome solution

---
+++ ExercismFSharp/ExercismFSharp/Palindrome.fs
@@ -0,0 +1,50 @@
+﻿module Palindrome
+
+let private getPalindromes lower upper =
+    let makeSeq n =
+        [n..upper]
+        |> Seq.map (fun m -> ((n, m), n * m))
+
+    let isPalindrome n =
+        let nString =
+            n
+            |> string
+        let reversedString =
+            nString
+            |> Seq.toArray
+            |> Array.rev
+            |> System.String
+        nString = reversedString
+
+    [lower..upper]
+    |> Seq.map makeSeq
+    |> Seq.concat
+    |> Seq.filter (fun ((_, _), a) -> isPalindrome a)
+
+let filterFactors factors =
+    let head =
+        factors
+        |> Seq.head
+        |> snd
+
+    let matchingFactors =
+        factors
+        |> Seq.takeWhile (fun ((_, _), a) -> a = head)
+        |> Seq.map (fun (a, _) -> a)
+        |> Seq.toList
+
+    (head, matchingFactors)
+
+let largestPalindrome lower upper =
+    let factors =
+        getPalindromes lower upper
+        |> Seq.sortByDescending (fun ((_, _), a) -> a)
+
+    filterFactors factors
+
+let smallestPalindrome lower upper =
+    let factors =
+        getPalindromes lower upper
+        |> Seq.sortBy (fun ((_, _), a) -> a)
+
+    filterFactors factors
