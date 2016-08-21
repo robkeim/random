b567b89e2d87ae3ce9523cea457a405c575f5c55
Rob Keim <robkeim@gmail.com>
2016-08-21 02:13:57 +0200

    [ExercismFSharp] Add Series solution

---
+++ ExercismFSharp/ExercismFSharp/Series.fs
@@ -0,0 +1,18 @@
+﻿module Series
+
+open System
+
+let slices (input : string) length =
+    let inputLength = input |> Seq.length
+
+    match inputLength with
+    | _ when length > inputLength -> raise (ArgumentException "length specified is too large")
+    | _ ->
+        [0 .. (input |> Seq.length) - length]
+        |> List.map (fun x ->
+            input
+            |> Seq.skip x
+            |> Seq.take length)
+        |> List.map (fun x ->
+            x
+            |> Seq.map (fun c -> Int32.Parse (c |> string)))
