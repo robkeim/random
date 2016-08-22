41b093c1640d52e8137beb73619fc205ceca7e41
Rob Keim <robkeim@gmail.com>
2016-08-23 00:28:24 +0200

    [ExercismFSharp] Add Sieve solution

---
+++ ExercismFSharp/ExercismFSharp/Sieve.fs
@@ -0,0 +1,13 @@
+﻿module Sieve
+
+let rec private listToFilter res list =
+    match list with
+    | [] -> res
+    | x::xs ->
+        let filtered =
+            xs |> List.filter (fun elem -> not (elem % x = 0))
+        listToFilter (x :: res) filtered
+
+let primesUpTo num =
+    listToFilter [] [2..num]
+    |> List.rev
