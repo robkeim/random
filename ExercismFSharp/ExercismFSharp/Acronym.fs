b17d4ef066884fcbb18ea1168108196d9c90f6e9
Rob Keim <robkeim@gmail.com>
2016-08-25 19:36:33 +0200

    [ExercismFSharp] Add Acronym solution

---
+++ ExercismFSharp/ExercismFSharp/Acronym.fs
@@ -0,0 +1,18 @@
+﻿module Acronym
+
+open System
+
+let acronym (input : string) =
+    let phrase =
+        input.Split [|':'|]
+        |> Array.head
+
+    phrase.Split [|' '; '-'|]
+    |> Seq.filter (fun word -> not (Seq.length word = 0))
+    |> Seq.map (fun word ->
+        Seq.append
+            (word |> Seq.head |> Char.ToUpperInvariant |> Seq.singleton)
+            (word |> Seq.tail |> Seq.filter Char.IsUpper)
+    )
+    |> Seq.concat
+    |> String.Concat
