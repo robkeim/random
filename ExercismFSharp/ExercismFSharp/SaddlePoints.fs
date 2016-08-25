13662b0ae052e497832620f657810028a26786b5
Rob Keim <robkeim@gmail.com>
2016-08-25 20:49:14 +0200

    [ExercismFSharp] Add SaddlePoints solution

---
+++ ExercismFSharp/ExercismFSharp/SaddlePoints.fs
@@ -0,0 +1,25 @@
+﻿module SaddlePoints
+
+let saddlePoints (matrix : int list list) =
+    let isValidRow value row =
+        matrix.[row]
+        |> Seq.forall (fun n -> value >= n)
+    
+    let isValidCol value col =
+        matrix
+        |> Seq.mapi(fun row _ -> matrix.[row].[col])
+        |> Seq.forall(fun n -> value <= n)
+
+    matrix
+    |> List.mapi (fun i _ ->
+        matrix.[i]
+        |> List.mapi (fun j _ ->
+            let value = matrix.[i].[j]
+
+            match ((isValidRow value i) && (isValidCol value j)) with
+            | true -> Some (i, j)
+            | _ -> None
+        )
+        |> List.choose id
+    )
+    |> List.collect id
