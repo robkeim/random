aea5c1724830106c8ccc375575335c7fc6d7476e
Rob Keim <robkeim@gmail.com>
2016-08-27 22:50:18 +0200

    [ExercismFSharp] Add GradeSchool solution

---
+++ ExercismFSharp/ExercismFSharp/GradeSchool.fs
@@ -0,0 +1,26 @@
+﻿module GradeSchool
+
+type School = { roster : (int * string) list }
+
+let empty = { roster = [] }
+
+let add name grade school =
+    { roster = (grade, name) :: school.roster }
+
+let grade grade school =
+    school.roster
+    |> List.filter (fun (g, _) -> g = grade)
+    |> List.map snd
+    |> List.sort
+
+let roster school =
+    school.roster
+    |> List.groupBy fst
+    |> List.map (fun (grade, values) ->
+        let namesInGrade =
+            values
+            |> List.map snd
+            |> List.sort
+        (grade, namesInGrade)
+    )
+    |> List.sort
