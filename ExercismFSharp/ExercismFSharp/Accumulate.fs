1b71c16b16e13eeb8b806c1fab94311f76f205af
Rob Keim <robkeim@gmail.com>
2016-08-27 12:37:31 +0200

    [ExercismFSharp] Add Accumulate solution

---
+++ ExercismFSharp/ExercismFSharp/Accumulate.fs
@@ -0,0 +1,9 @@
+﻿module Accumulate
+
+let rec private accumulateHelper f input state =
+    match input with
+    | [] -> state |> List.rev
+    | x::xs -> accumulateHelper f xs (f x :: state)
+
+let accumulate f input =
+    accumulateHelper f input []
