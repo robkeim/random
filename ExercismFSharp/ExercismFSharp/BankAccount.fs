4890d4c207eaeead14239b145f0ef226f2452d96
Rob Keim <robkeim@gmail.com>
2016-08-27 12:18:53 +0200

    [ExercismFSharp] Add BankAccount solution

---
+++ ExercismFSharp/ExercismFSharp/BankAccount.fs
@@ -0,0 +1,16 @@
+﻿module BankAccount
+
+type BankAccount = { balance : float; isOpen : bool }
+
+let mkBankAccount() = { balance = 0.0; isOpen = false }
+
+let openAccount account = { account with isOpen = true }
+
+let closeAccount account = { account with isOpen = false }
+
+let getBalance account =
+    match account.isOpen with
+    | true -> Some account.balance
+    | false -> None
+
+let updateBalance balance account = { account with balance = account.balance + balance }
