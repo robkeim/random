eaad85653765514b7b89027a80376a56ea7907a3
Rob Keim <robkeim@gmail.com>
2016-08-28 12:22:37 +0200

    [ExercismFSharp] Add ScoreScrabble solution

---
+++ ExercismFSharp/ExercismFSharp.Tests/ScrabbleScoreTest.fs
@@ -0,0 +1,32 @@
+﻿module ScrabbleScoreTest
+
+open NUnit.Framework
+open ScrabbleScore
+  
+[<Test>]
+let ``Empty word scores zero`` () =
+    Assert.That(score "", Is.EqualTo(0))
+
+[<Test>]
+let ``Whitespace scores zero`` () =
+    Assert.That(score " \t\n", Is.EqualTo(0))
+
+[<Test>]
+let ``Scores very short word`` () =
+    Assert.That(score "a", Is.EqualTo(1))
+
+[<Test>]
+let ``Scores other very short word`` () =
+    Assert.That(score "f", Is.EqualTo(4))
+
+[<Test>]
+let ``Simple word scores the number of letters`` () =
+    Assert.That(score "street", Is.EqualTo(6))
+
+[<Test>]
+let ``Complicated word scores more`` () =
+    Assert.That(score "quirky", Is.EqualTo(22))
+
+[<Test>]
+let ``Scores are case insensitive`` () =
+    Assert.That(score "OXYPHENBUTAZONE", Is.EqualTo(41))
---
+++ ExercismFSharp/ExercismFSharp/ScrabbleScore.fs
@@ -0,0 +1,18 @@
+﻿module ScrabbleScore
+
+open System
+
+let private getValueForLetter letter =
+    match Char.ToUpperInvariant letter with
+    | 'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' -> 1
+    | 'D' | 'G' -> 2
+    | 'B' | 'C' | 'M' | 'P' -> 3
+    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
+    | 'K' -> 5
+    | 'J' | 'X' -> 8
+    | 'Q' | 'Z' -> 10
+    | _ -> 0
+
+let score input =
+    input
+    |> Seq.sumBy getValueForLetter
