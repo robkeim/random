e5249290e49f69f2d5ef889300c7a4f3061632e3
Rob Keim <robkeim@gmail.com>
2016-08-28 16:08:34 +0200

    [ExercismFSharp] Add Strain solution

---
+++ ExercismFSharp/ExercismFSharp/Strain.fs
@@ -0,0 +1,21 @@
+﻿module Seq
+
+let rec private keepHelper pred input state =
+    match input with
+    | [] -> state |> List.rev
+    | x::xs -> match pred x with
+                | true -> keepHelper pred xs (x :: state)
+                | false -> keepHelper pred xs state
+
+let keep pred input =
+    keepHelper pred (input |> Seq.toList) List.empty
+
+let rec private discardHelper pred input state =
+    match input with
+    | [] -> state |> List.rev
+    | x::xs -> match pred x with
+                | false -> discardHelper pred xs (x :: state)
+                | true -> discardHelper pred xs state
+
+let discard pred input =
+    discardHelper pred (input |> Seq.toList) List.empty
