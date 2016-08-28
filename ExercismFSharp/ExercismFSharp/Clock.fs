1a01f12d7adcd9136f1972e8c06c2f1d367f3766
Rob Keim <robkeim@gmail.com>
2016-08-28 10:39:16 +0200

    [ExercismFSharp] Add Clock solution

---
+++ ExercismFSharp/ExercismFSharp/Clock.fs
@@ -0,0 +1,23 @@
+﻿module Clock
+
+type Clock = { minutes : int }
+
+let rec private modulo n m =
+    match n with
+    | _ when n < 0 -> modulo (n + m) m
+    | _ -> n % m
+
+let mkClock hours minutes =
+    { minutes = modulo (hours * 60 + minutes) 1440 }
+
+let display clock =
+    sprintf "%02i:%02i" (clock.minutes / 60) (clock.minutes % 60)
+
+let private updateTime op minutes offset =
+    modulo (op minutes offset) 1440
+
+let add minutes clock =
+    { minutes = updateTime (+) clock.minutes minutes }
+
+let subtract minutes clock =
+    { minutes = updateTime (-) clock.minutes minutes }
