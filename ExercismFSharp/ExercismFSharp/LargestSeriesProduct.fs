81cfc91f014fa14f0dd2adaf248db854a0830f94
Rob Keim <robkeim@gmail.com>
2016-08-29 22:04:50 +0200

    [ExercismFSharp] Add LargestSeriesProduct solution

---
+++ ExercismFSharp/ExercismFSharp/LargestSeriesProduct.fs
@@ -0,0 +1,18 @@
+﻿module LargestSeriesProduct
+
+open System
+
+let private toInt char =
+    char
+    |> string
+    |> Int32.Parse
+
+let largestProduct (digits : string) seriesLength =
+    match seriesLength with
+    | 0 -> 1
+    | _ ->
+            digits
+            |> Seq.windowed seriesLength
+            |> Seq.map (Seq.fold (fun acc elem -> acc * (toInt elem)) 1)
+            |> Seq.sort
+            |> Seq.max
