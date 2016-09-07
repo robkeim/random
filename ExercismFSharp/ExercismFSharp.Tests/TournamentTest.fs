7cdcdb6f2fbb79dddfcdbe4258394b2abc3c3b65
Rob Keim <robkeim@gmail.com>
2016-09-08 00:16:40 +0200

    [ExercismFSharp] Add Tournament solution

---
+++ ExercismFSharp/ExercismFSharp.Tests/TournamentTest.fs
@@ -0,0 +1,65 @@
+﻿module TournamentTest
+    
+open NUnit.Framework
+
+open Tournament
+
+[<Test>]
+let ``Correctly displays the tournament table`` () =
+    let actual = 
+        ["Αllegoric Alaskians;Blithering Badgers;win";
+         "Devastating Donkeys;Courageous Californians;draw";
+         "Devastating Donkeys;Αllegoric Alaskians;win";
+         "Courageous Californians;Blithering Badgers;loss";
+         "Blithering Badgers;Devastating Donkeys;loss";
+         "Αllegoric Alaskians;Courageous Californians;win"]
+
+    let expected = 
+        ["Team                           | MP |  W |  D |  L |  P";
+         "Devastating Donkeys            |  3 |  2 |  1 |  0 |  7";
+         "Αllegoric Alaskians            |  3 |  2 |  0 |  1 |  6";
+         "Blithering Badgers             |  3 |  1 |  0 |  2 |  3";
+         "Courageous Californians        |  3 |  0 |  1 |  2 |  1"]
+
+    Assert.That(tally actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Ignores incorrect input`` () =
+    let actual = 
+        ["Allegoric Alaskians;Blithering Badgers;win";
+         "Devastating Donkeys_Courageous Californians;draw";
+         "Devastating Donkeys;Allegoric Alaskians;win";
+         "";
+         "Courageous Californians;Blithering Badgers;loss";
+         "Bla;Bla;Bla";
+         "Blithering Badgers;Devastating Donkeys;loss";
+         "# Yackity yackity yack";
+         "Allegoric Alaskians;Courageous Californians;win";
+         "Devastating Donkeys;Courageous Californians;draw";
+         "Devastating Donkeys@Courageous Californians;draw"]
+
+    let expected = 
+        ["Team                           | MP |  W |  D |  L |  P";
+         "Devastating Donkeys            |  3 |  2 |  1 |  0 |  7";
+         "Allegoric Alaskians            |  3 |  2 |  0 |  1 |  6";
+         "Blithering Badgers             |  3 |  1 |  0 |  2 |  3";
+         "Courageous Californians        |  3 |  0 |  1 |  2 |  1"]
+
+    Assert.That(tally actual, Is.EqualTo(expected))    
+
+[<Test>]
+let ``Correctly displays another tournament table`` () =
+    let actual = 
+        ["Allegoric Alaskians;Blithering Badgers;win";
+         "Devastating Donkeys;Allegoric Alaskians;win";
+         "Courageous Californians;Blithering Badgers;loss";
+         "Allegoric Alaskians;Courageous Californians;win"]
+
+    let expected = 
+        ["Team                           | MP |  W |  D |  L |  P";
+         "Allegoric Alaskians            |  3 |  2 |  0 |  1 |  6";
+         "Blithering Badgers             |  2 |  1 |  0 |  1 |  3";
+         "Devastating Donkeys            |  1 |  1 |  0 |  0 |  3";
+         "Courageous Californians        |  2 |  0 |  0 |  2 |  0"]
+
+    Assert.That(tally actual, Is.EqualTo(expected))
---
+++ ExercismFSharp/ExercismFSharp/Tournament.fs
@@ -0,0 +1,46 @@
+﻿module Tournament
+
+type private Result =
+    | Win
+    | Draw
+    | Loss
+
+type private TeamRecord =
+    {
+        name : string
+        matchesPlayed : int
+        wins : int
+        draws : int
+        losses : int
+        points : int
+    }
+
+let private parseLine (line : string) =
+    let foo =
+        match line.Split[|';'|] with
+        | [|a; b; "win"|]  -> Some [(a, Result.Win); (b, Result.Loss)]
+        | [|a; b; "draw"|] -> Some [(a, Result.Draw); (b, Result.Draw)]
+        | [|a; b; "loss"|] -> Some [(a, Result.Loss); (b, Result.Win)]
+        | _ -> None
+    foo
+
+let tally input =
+    let results =
+        input
+        |> List.choose parseLine
+        |> List.concat
+        |> List.groupBy fst
+        |> List.map (fun (key, value) -> (key, value |> List.map snd))
+        |> List.map (fun (name, results) ->
+            let mp = results |> List.length
+            let w = results |> List.filter (fun r -> r = Result.Win) |> List.length
+            let d = results |> List.filter (fun r -> r = Result.Draw) |> List.length
+            let l = results |> List.filter (fun r -> r = Result.Loss) |> List.length
+            let p = 3 * w + d
+            { name = name; matchesPlayed = mp; wins = w; draws = d; losses = l; points = p })
+        |> List.sortBy(fun record -> (-1 * record.points, record.name))
+        |> List.map (fun record -> (sprintf "%-31s| %2i | %2i | %2i | %2i | %2i" record.name record.matchesPlayed record.wins record.draws record.losses record.points))
+
+    let header = "Team                           | MP |  W |  D |  L |  P"
+    
+    header::results
