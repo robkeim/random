587a83cec3e15bc531aa42424cecf0c5bfee3111
Rob Keim <robkeim@gmail.com>
2016-09-13 23:25:56 +0200

    [ExercismFSharp] Add BracketPush solution

---
+++ ExercismFSharp/ExercismFSharp/BracketPush.fs
@@ -0,0 +1,31 @@
+﻿module BracketPush
+
+open System
+
+let private matchClosingBracket closingBracket stack =
+    let bracket =
+        match closingBracket with
+        | ')' -> '('
+        | '}' -> '{'
+        | ']' -> '['
+        | _ -> raise (Exception "unreachable code")
+
+    match stack with
+    | [] -> false
+    | x::xs when x = bracket -> true
+    | _ -> false
+
+let rec private matchedHelper input brackets =
+    match input with
+    | [] -> Seq.length brackets = 0
+    | x::xs ->
+        match x with
+        | '(' | '{' | '[' -> matchedHelper xs (x :: brackets)
+        | ')' | '}' | ']' ->
+            match matchClosingBracket x brackets with
+            | true  -> matchedHelper xs brackets.[1..]
+            | false -> false
+        | _ -> matchedHelper xs brackets
+
+let matched input =
+    matchedHelper (Seq.toList input) []
