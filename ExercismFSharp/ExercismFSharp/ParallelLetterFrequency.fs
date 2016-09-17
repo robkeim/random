127b7cd251d30b8832445cae1c4d3b600cc5ae24
Rob Keim <robkeim@gmail.com>
2016-09-17 18:56:56 +0200

    [ExercismFSharp] Add ParallelLetterFrequency solution

---
+++ ExercismFSharp/ExercismFSharp/ParallelLetterFrequency.fs
@@ -0,0 +1,24 @@
+﻿module ParallelLetterFrequency
+
+open System
+
+let private countFrequencies (text : string) =
+    text.ToLowerInvariant()
+    |> Seq.filter Char.IsLetter
+    |> Seq.countBy id
+    |> Map.ofSeq
+
+let mergeMap (map : Map<char, int>) =
+    map
+    |> Map.fold (fun acc k v ->
+        match Map.tryFind k acc with
+        | Some v' -> acc.Add(k, v + v')
+        | None -> acc.Add(k, v)
+    )
+
+let frequency input =
+    input
+    |> Seq.map (fun text -> async { return countFrequencies text })
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.fold mergeMap Map.empty
