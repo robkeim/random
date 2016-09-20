12ec9241504cc3a9cd334823f562e12e827e9172
Rob Keim <robkeim@gmail.com>
2016-09-20 22:03:11 +0200

    [ProjectEuler] Add 25 solution

--- ProjectEuler/ProjectEuler/Program.fs
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -5,6 +5,7 @@ open System
 [<EntryPoint>]
 let main argv =
     //Question19.numSundays
+    Question25.thousandDigitFibonacci
 
     printfn "done!"
     Console.ReadLine() |> ignore
---
+++ ProjectEuler/ProjectEuler/Question25.fs
@@ -0,0 +1,23 @@
+﻿module Question25
+
+open System
+
+let thousandDigitFibonacci =
+    let fibs =  
+        let rec f a b = seq { yield a
+                              yield! f b (a+b) }
+        f 1I 1I
+        |> Seq.cache
+
+    let num =
+        [2..1000]
+        |> List.fold (fun acc _ -> acc * 10I) 1I
+
+    let result =
+        fibs
+        |> Seq.takeWhile (fun value -> value < num)
+        |> Seq.length
+
+    Console.WriteLine (result + 1)
+
+    ()
