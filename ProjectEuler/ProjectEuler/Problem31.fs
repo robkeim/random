92be95bc6aed1163b372efc9d635779c267321fb
Rob Keim <robkeim@gmail.com>
2016-09-20 22:46:28 +0200

    [ProjectEuler] Add 31 solution

---
+++ ProjectEuler/ProjectEuler/Problem31.fs
@@ -0,0 +1,16 @@
+﻿module Problem31
+
+open System
+
+let rec private countChange value coins =
+    match coins, value with
+    | _, 0 -> 1
+    | [], _ -> 0
+    | _, _ when value < 0 -> 0
+    | x::xs, _ -> (countChange (value - x) coins) + (countChange value xs)
+
+let numCoins =
+    let result = countChange 200 [ 1; 2; 5; 10; 20; 50; 100; 200 ]
+
+    Console.WriteLine result
+    ()
--- ProjectEuler/ProjectEuler/Program.fs
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -6,7 +6,8 @@ open System
 let main argv =
     //Problem19.numSundays
     //Problem25.thousandDigitFibonacci
-    Problem30.digitFifthPowers
+    //Problem30.digitFifthPowers
+    Problem31.numCoins
 
     printfn "done!"
     Console.ReadLine() |> ignore
