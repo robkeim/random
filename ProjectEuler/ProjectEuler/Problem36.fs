0e30519c3e7f203182278a50fd5aaecb86395dfc
Rob Keim <robkeim@gmail.com>
2016-09-24 23:49:40 +0200

    [ProjectEuler] Add 36 solution

---
+++ ProjectEuler/ProjectEuler/Problem36.fs
@@ -0,0 +1,26 @@
+﻿module Problem36
+
+open System
+
+let private reverse (str : string) =
+    str
+    |> Seq.rev
+    |> Seq.fold (sprintf "%s%c") String.Empty
+
+let private isPalindrome n =
+    (n |> Seq.rev |> Seq.head <> '0') && (n = reverse n)
+
+let rec private toBinary outputDigits number =
+    match number with
+    | 0 -> outputDigits |> List.fold (sprintf "%s%i") String.Empty
+    | _ -> toBinary ((number % 2) :: outputDigits) (number / 2)
+
+let doublePalendromes =
+    let result =
+        [1..999999]
+        |> List.filter (fun n -> isPalindrome (n |> string))
+        |> List.filter (fun n -> isPalindrome (toBinary [] n))
+        |> List.sum
+
+    Console.WriteLine result
+    ()
--- ProjectEuler/ProjectEuler/Program.fs
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -7,7 +7,8 @@ let main argv =
     //Problem19.numSundays
     //Problem25.thousandDigitFibonacci
     //Problem30.digitFifthPowers
-    Problem31.numCoins
+    //Problem31.numCoins
+    Problem36.doublePalendromes
 
     printfn "done!"
     Console.ReadLine() |> ignore
