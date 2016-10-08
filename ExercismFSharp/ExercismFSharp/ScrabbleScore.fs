3952ef935d9cbfa6518a1324070a5f5d23829ce3
Rob Keim <robkeim@gmail.com>
2016-10-08 17:29:36 +0200

    [ExercismFSharp] Cleanup ScrabbleScore solution

--- ExercismFSharp/ExercismFSharp/ScrabbleScore.fs
+++ ExercismFSharp/ExercismFSharp/ScrabbleScore.fs
@@ -4,14 +4,14 @@ open System
 
 let private getValueForLetter letter =
     match Char.ToUpperInvariant letter with
-    | 'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' -> 1
-    | 'D' | 'G' -> 2
-    | 'B' | 'C' | 'M' | 'P' -> 3
-    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
-    | 'K' -> 5
-    | 'J' | 'X' -> 8
-    | 'Q' | 'Z' -> 10
-    | _ -> 0
+    | 'A' | 'E' | 'I' | 'O' | 'U'| 'L' | 'N' | 'R' | 'S' | 'T' -> 1
+    | 'D' | 'G'                                                 -> 2
+    | 'B' | 'C' | 'M' | 'P'                                     -> 3
+    | 'F' | 'H' | 'V' | 'W' | 'Y'                               -> 4
+    | 'K'                                                       -> 5
+    | 'J' | 'X'                                                 -> 8
+    | 'Q' | 'Z'                                                 -> 10
+    | _                                                         -> 0
 
 let score input =
     input
