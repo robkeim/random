191dd6f3c4611da9a791d9e0d288e19d8c49bd0c
Rob Keim <robkeim@gmail.com>
2016-10-08 18:19:39 +0200

    [ExercismFSharp] Simplify QueenAttack solution

--- ExercismFSharp/ExercismFSharp/QueenAttack.fs
+++ ExercismFSharp/ExercismFSharp/QueenAttack.fs
@@ -4,8 +4,8 @@ open System
 
 let canAttack white black =
     match (white : int * int), black with
-    | (x1, y1), (x2, y2) when x1 = x2 && y1 = y2 -> raise (Exception "cannot occupy the same space")
-    | (x1, _), (x2, _) when x1 = x2 -> true
-    | (_, y1), (_, y2) when y1 = y2 -> true
+    | (x1, y1), (x2, y2) when x1 = x2 && y1 = y2                    -> failwith "cannot occupy the same space"
+    | (x1, _), (x2, _) when x1 = x2                                 -> true
+    | (_, y1), (_, y2) when y1 = y2                                 -> true
     | (x1, y1), (x2, y2) when Math.Abs(x1 - x2) = Math.Abs(y1 - y2) -> true
-    | _ -> false
+    | _                                                             -> false
