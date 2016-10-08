6c0fc3870f64814e42a383312c5359b9f9314b65
Rob Keim <robkeim@gmail.com>
2016-10-08 18:48:03 +0200

    [ExercismFSharp] Simplify Series solution

--- ExercismFSharp/ExercismFSharp/Series.fs
+++ ExercismFSharp/ExercismFSharp/Series.fs
@@ -6,13 +6,11 @@ let slices (input : string) length =
     let inputLength = input |> Seq.length
 
     match inputLength with
-    | _ when length > inputLength -> raise (ArgumentException "length specified is too large")
+    | _ when length > inputLength -> failwith "length specified is too large"
     | _ ->
         [0 .. (input |> Seq.length) - length]
         |> List.map (fun x ->
             input
             |> Seq.skip x
             |> Seq.take length)
-        |> List.map (fun x ->
-            x
-            |> Seq.map (fun c -> Int32.Parse (c |> string)))
+        |> List.map (Seq.map (fun c -> Int32.Parse (c |> string)))
