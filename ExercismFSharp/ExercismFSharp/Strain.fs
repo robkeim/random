7a3e5385288446934753d17cd92ac9501771b2ba
Rob Keim <robkeim@gmail.com>
2016-10-08 18:22:51 +0200

    [ExercismFSharp] Simplify Strain solution

--- ExercismFSharp/ExercismFSharp/Strain.fs
+++ ExercismFSharp/ExercismFSharp/Strain.fs
@@ -2,20 +2,22 @@
 
 let rec private keepHelper pred input state =
     match input with
-    | [] -> state |> List.rev
-    | x::xs -> match pred x with
-                | true -> keepHelper pred xs (x :: state)
-                | false -> keepHelper pred xs state
+    | []    -> state |> List.rev
+    | x::xs -> if pred x then
+                   keepHelper pred xs (x :: state)
+               else
+                   keepHelper pred xs state
 
 let keep pred input =
     keepHelper pred (input |> Seq.toList) List.empty
 
 let rec private discardHelper pred input state =
     match input with
-    | [] -> state |> List.rev
-    | x::xs -> match pred x with
-                | false -> discardHelper pred xs (x :: state)
-                | true -> discardHelper pred xs state
+    | []    -> state |> List.rev
+    | x::xs -> if pred x then
+                   discardHelper pred xs state
+               else
+                   discardHelper pred xs (x :: state)
 
 let discard pred input =
     discardHelper pred (input |> Seq.toList) List.empty
