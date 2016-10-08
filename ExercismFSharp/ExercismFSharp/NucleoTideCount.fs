0aa6919a307d479c6f55a563f4da424073e38ef3
Rob Keim <robkeim@gmail.com>
2016-10-08 14:03:23 +0200

    [ExercismFSharp] Simplify NucleotideCount solution

--- ExercismFSharp/ExercismFSharp/NucleoTideCount.fs
+++ ExercismFSharp/ExercismFSharp/NucleoTideCount.fs
@@ -1,14 +1,12 @@
 ﻿module NucleoTideCount
 
-open System
-
 let count nucleotide strand =
     match nucleotide with
     | 'A' | 'T' | 'C' | 'G' -> 
         strand
         |> Seq.filter (fun c -> c = nucleotide)
         |> Seq.length
-    | _ -> raise (Exception "unknown nucleotide type")
+    | _ -> failwith "unknown nucleotide type"
 
 let nucleotideCounts strand =
     [ 'A'; 'T'; 'C'; 'G']
