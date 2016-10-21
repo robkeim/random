1423bbe081b117171ed748d4d932d5c25c6b8964
Rob Keim <robkeim@gmail.com>
2016-10-21 20:15:35 +0200

    [GameOfLife] Add empty World

---
+++ GameOfLife/GameOfLife/World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    // TODO override GetHashCode
+    public class World: IEquatable<World>
+    {
+        public bool Equals(World other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as World);
+        }
+    }
+}
---
+++ GameOfLife/GameOfLifeTests/WorldTests.cs
@@ -0,0 +1,32 @@
+﻿using GameOfLife;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class WorldTests
+    {
+        [TestMethod]
+        public void Two_Worlds_With_The_Same_Reference_Are_Equal()
+        {
+            // Arrange
+            var world = new World();
+
+            // Act
+            // Assert
+            Assert.AreEqual(world, world);
+        }
+
+        [TestMethod]
+        public void Two_Worlds_With_Different_References_Are_Equal()
+        {
+            // Arrange
+            var world1 = new World();
+            var world2 = new World();
+
+            // Act
+            // Assert
+            Assert.AreEqual(world1, world2);
+        }
+    }
+}
