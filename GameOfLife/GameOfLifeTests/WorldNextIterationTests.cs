94655a35f6aa06675f4007285580d7acd60284ef
Rob Keim <robkeim@gmail.com>
2016-10-22 17:38:30 +0200

    [GameOfLife] Add additional tests for GetNextIteration

--- GameOfLife/GameOfLifeTests/WorldNextIterationTests.cs
+++ GameOfLife/GameOfLifeTests/WorldNextIterationTests.cs
@@ -16,12 +16,52 @@ namespace GameOfLifeTests
                 ".");
 
             // Act
-            var actual = world.GetNextIteration();
+            var actual = world
+                .GetNextIteration()
+                .GetCurrentState();
 
             // Assert
-            TestUtils.AssertWorldStatesAreEqual(expected, actual.GetCurrentState());
+            TestUtils.AssertWorldStatesAreEqual(expected, actual);
         }
 
-        // TODO add test to verify that world expands as needed
+        [TestMethod]
+        public void Dead_Cell_With_Three_Neighbors_Changes_State_In_Next_Iteration()
+        {
+            // Arrange
+            var world = TestUtils.CreateWorld(
+                "X..",
+                "X.X");
+            var expected = TestUtils.CreateExpectedWorldState(
+                ".X.",
+                ".X.");
+
+            // Act
+            var actual = world
+                .GetNextIteration()
+                .GetCurrentState();
+
+            // Assert
+            TestUtils.AssertWorldStatesAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void World_Expands_In_Next_Iteration()
+        {
+            // Arrange
+            var world = TestUtils.CreateWorld(
+                "XXX");
+            var expected = TestUtils.CreateExpectedWorldState(
+                ".X.",
+                ".X.",
+                ".X.");
+
+            // Act
+            var actual = world
+                .GetNextIteration()
+                .GetCurrentState();
+
+            // Assert
+            TestUtils.AssertWorldStatesAreEqual(expected, actual);
+        }
     }
 }
