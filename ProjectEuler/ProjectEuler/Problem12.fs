cf51da0dd078be9380efa6a7ee7535433ea304f7
Rob Keim <robkeim@gmail.com>
2016-12-29 06:41:42 +0100

    [ProjectEuler] Add 12 solution

---
+++ ProjectEuler/ProjectEuler/Problem12.fs
@@ -0,0 +1,30 @@
+﻿module Problem12
+
+open System
+
+let triangleNumber =
+    let triangleNumbers =  
+        Seq.initInfinite (fun i ->
+            i * (i + 1) / 2
+            )
+        |> Seq.cache
+
+    let countFactors n =
+        let sqrt = Math.Sqrt (n |> float) |> int
+        
+        let numFactors =
+            [1 .. sqrt]
+            |> Seq.filter (fun i -> n % i = 0)
+            |> Seq.length
+
+        match sqrt * sqrt = n with
+        | true  -> 2 * numFactors - 1
+        | false -> 2 * numFactors
+
+    let result =
+        triangleNumbers
+        |> Seq.filter (fun i -> countFactors i > 500)
+        |> Seq.head
+
+    Console.WriteLine result
+    ()
--- ProjectEuler/ProjectEuler/Program.fs
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -4,13 +4,14 @@ open System
 
 [<EntryPoint>]
 let main argv =
+    //Problem12.triangleNumber
     //Problem19.numSundays
     //Problem25.thousandDigitFibonacci
     //Problem30.digitFifthPowers
     //Problem31.numCoins
     //Problem36.doublePalendromes
     //Problem42.numTriangleWords
-    Problem52.permutedMultiples
+    //Problem52.permutedMultiples
 
     printfn "done!"
     Console.ReadLine() |> ignore
