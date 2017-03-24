fd65a3f178cd52b4b33875222e136cffc0c6c8cf
Rob Keim <robkeim@gmail.com>
2017-03-24 19:40:43 +0700

    [DateFormatTester] Create program to test DateTime formats in different languages

---
+++ DateFormatTester/DateFormatTester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace DateFormatTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new DateTime(2017, 3, 20);
+            var format = "MMMMd日";
+            var locales = new[] { "en-US", "fr-FR", "ja-JP", "th-TH", "zh-CHS" };
+
+            foreach (var locale in locales)
+            {
+                var cultureInfo = new CultureInfo(locale);
+                var formattedDate = date.ToString(format, cultureInfo);
+                Console.WriteLine($"{locale}: {formattedDate}");
+            }
+
+            Console.WriteLine("\ndone!");
+            Console.ReadLine();
+        }
+    }
+}
