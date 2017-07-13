56fedadb1fc9836542254c0fd70445c881b35952
Rob Keim <robkeim@gmail.com>
2017-07-13 22:47:20 +0700

    [RserveGymClasses] Finish v1

---
+++ ReserveGymClasses/ReserveGymClasses/Email.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ReserveGymClasses
+{
+    public enum EmailStatus
+    {
+        Enabled,
+        OnlyToMe,
+        Disabled
+    }
+
+    public static class Email
+    {
+        private static readonly object lockObj = new object();
+
+        private static Task SendEmailAsync(string email, string subject, string body)
+        {
+            return SendEmailAsync(new[] { email }, subject, body);
+        }
+
+        public static Task SendEmailAsync(IEnumerable<string> rawTo, string subject, string body)
+        {
+            var to = rawTo.OrderBy(email => email).Distinct().ToList();
+            
+            lock (lockObj)
+            {
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine("To: {0}", string.Join(",", to));
+                Console.WriteLine("Subject: {0}", subject);
+                Console.WriteLine("Body:");
+                Console.WriteLine(body.Replace("<br />", "\n").Replace("<b>", "").Replace("</b>", "").Replace("&nbsp;", ""));
+                Console.WriteLine("--------------------------------------------------------------");
+            }
+
+            if (Program.EmailStatus != EmailStatus.Disabled)
+            {
+                if (Program.EmailStatus == EmailStatus.OnlyToMe)
+                {
+                    to = new List<string> { "robkeim@gmail.com" };
+                }
+
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential("robkeim@gmail.com", ConfigurationManager.AppSettings["GmailPassword"]),
+                    EnableSsl = true
+                };
+
+                var message = new MailMessage
+                {
+                    From = new MailAddress("robkeim@gmail.com"),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                };
+
+                foreach (var email in to)
+                {
+                    message.To.Add(new MailAddress(email));
+                }
+
+                return client.SendMailAsync(message);
+            }
+            else
+            {
+                Console.WriteLine("Email sending disabled");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- ReserveGymClasses/ReserveGymClasses/Program.cs
+++ ReserveGymClasses/ReserveGymClasses/Program.cs
@@ -4,28 +4,49 @@ using System.Linq;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace ReserveGymClasses
 {
     public class Program
     {
+        public static EmailStatus EmailStatus = EmailStatus.Enabled;
+
         public static void Main(string[] args)
         {
-            using (var driver = new ChromeDriver())
+            try
             {
-                var username = ConfigurationManager.AppSettings["Username"];
-                var password = ConfigurationManager.AppSettings["Password"];
+                using (var driver = new ChromeDriver())
+                {
+                    var username = ConfigurationManager.AppSettings["Username"];
+                    var password = ConfigurationManager.AppSettings["Password"];
 
-                driver.GoToUrlAndWaitForPageLoad("https://mylocker.virginactive.co.th/#/bookaclass");
-                driver.Login(username: username, usernameSelector: "#memberID",
-                    password: password, passwordSelector: "#password", submitButtonSelector: "form[name='loginForm'] button");
+                    driver.GoToUrlAndWaitForPageLoad("https://mylocker.virginactive.co.th/#/bookaclass");
+                    driver.Login(username: username, usernameSelector: "#memberID",
+                        password: password, passwordSelector: "#password", submitButtonSelector: "form[name='loginForm'] button");
 
-                var dayToSelect = DateTime.Now.Day + 1;
+                    var dayToSelect = DateTime.Now.Day + 6;
 
-                SelectDay(driver, dayToSelect);
+                    SelectDay(driver, dayToSelect);
+                    var classToReserve = FindClass(driver);
 
-                Console.WriteLine("done!");
-                Console.ReadLine();
+                    if (classToReserve != null)
+                    {
+                        ReserveClass(driver, classToReserve);
+                        SendEmail();
+                    }
+                    else
+                    {
+                        SendEmail(noClassAvailable: true);
+                    }
+
+                    Console.WriteLine("done!");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                SendEmail(success: false, exception: e);
             }
         }
 
@@ -47,6 +68,79 @@ namespace ReserveGymClasses
             element = element.GetParent();
             element.Click();
             driver.WaitForPageLoad();
+        }
+
+        private static IWebElement FindClass(ChromeDriver driver)
+        {
+            IReadOnlyCollection<IWebElement> elements = null;
+
+            Func<bool> func = () =>
+            {
+                elements = driver.FindElementsByCssSelector("tr.classRow");
+
+                return elements.Count != 0;
+            };
+
+            DriverExtensions.RetryUntilSuccess(func);
+
+            var minutesRegex = new Regex(@"12:(\d{2})pm", RegexOptions.Compiled);
+            
+            var validClass = elements.FirstOrDefault(e => {
+                var innerText = e.GetAttribute("innerText");
+
+                if (!innerText.Contains("Pilates Reformer"))
+                {
+                    return false;
+                }
+
+                var match = minutesRegex.Match(innerText);
+
+                if (!match.Success)
+                {
+                    return false;
+                }
+
+                var minutes = int.Parse(match.Groups[1].ToString());
+                
+                return minutes >= 15 && minutes <= 45;
+            });
+
+            return validClass;
+        }
+
+        private static void ReserveClass(ChromeDriver driver, IWebElement classToReserve)
+        {
+            classToReserve.Click();
+
+            var tmp = classToReserve.FindElement(By.XPath(@"./following-sibling::*"));
+            var linkToPopup = tmp.FindElement(By.CssSelector("a"));
+            linkToPopup.Click();
+
+            tmp = driver.FindElementByCssSelector("div.modal-dialog");
+            var book = tmp.FindElement(By.CssSelector("button[ng-click='vm.makeBooking()']"));
+
+            DriverExtensions.RetryUntilSuccess(() => book.Click());
+        }
+
+        private static void SendEmail(bool success = true, bool noClassAvailable = false, Exception exception = null)
+        {
+            var today = DateTime.Now.AddDays(6).ToString("MMM d");
+            var subject = $"[VirginActive][{today}] ";
+
+            if (noClassAvailable)
+            {
+                subject += "No classes available";
+            }
+            else if (success)
+            {
+                subject += "Success";
+            }
+            else
+            {
+                subject += "Error";
+            }
+
+            Email.SendEmailAsync(new[] { "robkeim@gmail.com" }, subject, exception?.ToString()).Wait();
         }
     }
 }
