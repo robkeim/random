74612e742d246e40de4f82de004774b4577ec4e6
Rob Keim <robkeim@gmail.com>
2017-08-28 13:39:02 +0700

    [Microsoft training] Add custom middleware

---
+++ MicrosoftTraining/Module 01 - Overview/MiddlewareDemo/MiddlewareDemo/RequestLoggerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Owin.Logging;
+using Owin;
+
+namespace MiddlewareDemo
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class RequestLoggerMiddleware
+    {
+        private readonly ILogger _logger;
+        AppFunc _next;
+        public RequestLoggerMiddleware(AppFunc next, IAppBuilder app)
+        {
+            _next = next;
+            _logger = app.CreateLogger<RequestLoggerMiddleware>();
+        }
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            _logger.WriteInformation($"Handling request: {environment["owin.RequestPath"]}");
+            await _next(environment);
+            _logger.WriteInformation("Finished handling request.");
+
+        }
+    }
+}
