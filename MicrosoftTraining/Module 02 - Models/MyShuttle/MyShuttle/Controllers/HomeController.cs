afa966ed19597524f71341de6c69809a17bb311c
Rob Keim <robkeim@gmail.com>
2017-08-28 15:59:04 +0700

    [Microsoft training] Module 3 - setup controllers

---
+++ MicrosoftTraining/Module 02 - Models/MyShuttle/MyShuttle/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using MyShuttle.Data;
+using MyShuttle.Model;
+using MyShuttle.Web.Models;
+
+namespace MyShuttle.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        ICarrierRepository _carrierRepository;
+
+        public HomeController(ICarrierRepository carrierRepository)
+        {
+            _carrierRepository = carrierRepository;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new MyShuttleViewModel()
+            {
+                MainMessage = "The Ultimate B2B Shuttle Service Solution"
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<int> Post(Carrier carrier)
+        {
+            return await _carrierRepository.AddAsync(carrier);
+        }
+    }
+}
---
+++ MicrosoftTraining/Module 02 - Models/MyShuttle/MyShuttle/Models/MyShuttleViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MyShuttle.Web.Models
+{
+    public class MyShuttleViewModel
+    {
+        public string MainMessage { get; set; }
+    }
+}
