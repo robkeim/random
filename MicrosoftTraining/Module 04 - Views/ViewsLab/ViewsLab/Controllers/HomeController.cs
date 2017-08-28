9a90c61db3f6598d3ddb31f45902e9baeeaed95b
Rob Keim <robkeim@gmail.com>
2017-08-28 20:57:42 +0700

    [Microsoft training] Module 4 - Play around with views

---
+++ MicrosoftTraining/Module 04 - Views/ViewsLab/ViewsLab/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "I am learning ASP.NET MVC!";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            var movie = new Models.Movie
+            {
+                ID = 1,
+                Title = "Follow the Wind",
+                ReleaseDate = new DateTime(2017, 01, 21)
+            };
+            return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult About(Models.Movie movieIncoming)
+        {
+            // Your logic here
+
+            return View(movieIncoming);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MicrosoftTraining/Module 04 - Views/ViewsLab/ViewsLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViewsLab.Startup))]
+namespace ViewsLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
