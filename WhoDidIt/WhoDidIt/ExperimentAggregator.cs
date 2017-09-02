995db6d72e0ff3756c63da432cf32e82eb1e9209
Rob Keim <robkeim@gmail.com>
2017-09-02 17:51:36 +0700

    [WhoDidIt] Add hard-coded solution

---
+++ WhoDidIt/WhoDidIt/ExperimentAggregator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhoDidIt
+{
+    public class ExperimentAggregator
+    {
+        Dictionary<string, int> _experiments = new Dictionary<string, int>();
+
+        public void AddExperimentRun(bool bugReproduced, IEnumerable<string> experimentsToAdd)
+        {
+            foreach (var experiment in experimentsToAdd)
+            {
+                if (!_experiments.ContainsKey(experiment))
+                {
+                    _experiments[experiment] = bugReproduced ? 1 : -1;
+                }
+                else
+                {
+                    if (bugReproduced)
+                    {
+                        _experiments[experiment]++;
+                    }
+                    else
+                    {
+                        _experiments[experiment]--;
+                    }
+                }
+            }
+        }
+
+        public void PrintStatistics()
+        {
+            var entries = _experiments.GroupBy(exp => exp.Value)
+                  .ToDictionary(grp => grp.Key, g => g.Select(kvp => kvp.Key).OrderBy(v => v).ToArray())
+                  .OrderByDescending(kvp => kvp.Key)
+                  .ToArray();
+
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"{entry.Key}\t{string.Join(", ", entry.Value)}");
+            }
+        }
+    }
+}
---
+++ WhoDidIt/WhoDidIt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WhoDidIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var experimentAggregator = new ExperimentAggregator();
+
+            experimentAggregator.AddExperimentRun(true, new[] { "HALO-123", "TEXT-1331" });
+            experimentAggregator.AddExperimentRun(false, new[] { "FAM-123", "TEXT-2323" });
+            experimentAggregator.AddExperimentRun(true, new[] { "FAM-123", "TEXT-1331" });
+            experimentAggregator.AddExperimentRun(false, new[] { "HALO-123", "CMS-111" });
+
+            experimentAggregator.PrintStatistics();
+
+            Console.WriteLine("done!");
+            Console.ReadLine();
+        }
+    }
+}
