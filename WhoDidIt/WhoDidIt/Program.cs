6eb711361da5285cceeb3dc66ef8a9f019c2957e
Rob Keim <robkeim@gmail.com>
2017-09-02 18:44:28 +0700

    [WhoDidIt] Process input from a while

--- WhoDidIt/WhoDidIt/Program.cs
+++ WhoDidIt/WhoDidIt/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace WhoDidIt
 {
@@ -6,17 +9,52 @@ namespace WhoDidIt
     {
         static void Main(string[] args)
         {
+            if (Debugger.IsAttached)
+            {
+                args = new[] { "./sampleInput.txt" };
+            }
+
+            if (args.Length == 0 || args.Length > 1)
+            {
+                Console.WriteLine("Usage: WhoDidIt.exe <input file>");
+                Console.WriteLine();
+                Console.WriteLine("Input file lines have the following format:");
+                Console.WriteLine("true|false <whitespace delimited list of experiments");
+                Console.WriteLine("ex: true HALO-123 TEXT-456 FAM-789");
+                return;
+            }
+
+            var inputFile = args[0];
+
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine($"Cannot find file: {inputFile}. Please provide a valid file");
+                return;
+            }
+
             var experimentAggregator = new ExperimentAggregator();
 
-            experimentAggregator.AddExperimentRun(true, new[] { "HALO-123", "TEXT-1331" });
-            experimentAggregator.AddExperimentRun(false, new[] { "FAM-123", "TEXT-2323" });
-            experimentAggregator.AddExperimentRun(true, new[] { "FAM-123", "TEXT-1331" });
-            experimentAggregator.AddExperimentRun(false, new[] { "HALO-123", "CMS-111" });
+            foreach (var line in File.ReadAllLines(inputFile))
+            {
+                var splitLine = line.Split(" \t".ToCharArray());
+                bool bugReproduced;
+
+                if (!bool.TryParse(splitLine[0], out bugReproduced))
+                {
+                    Console.WriteLine($"Line not correct formatted, could not determine if the bug reproduced: {line}");
+                    return;
+                }
+
+                experimentAggregator.AddExperimentRun(bugReproduced, splitLine.Skip(1));
+            }
 
             experimentAggregator.PrintStatistics();
-
-            Console.WriteLine("done!");
-            Console.ReadLine();
+            
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("done!");
+                Console.ReadLine();
+            }
         }
     }
 }
