bbb7df69e130c01a6fa4f01bee5e698bfbf03c90
Rob Keim <robkeim@gmail.com>
2017-12-13 22:28:34 +0700

    [AdventOfCode] 2017 Add Day 13 Part 2 solution

--- AdventOfCode/2017/day13.js
+++ AdventOfCode/2017/day13.js
@@ -160,6 +160,121 @@ In this situation, you are caught in layers 0 and 6, because your packet entered
 The severity of getting caught on a layer is equal to its depth multiplied by its range. (Ignore layers in which you do not get caught.) The severity of the whole trip is the sum of these values. In the example above, the trip severity is 0*3 + 6*4 = 24.
 
 Given the details of the firewall you've recorded, if you leave immediately, what is the severity of your whole trip?
+
+--- Part Two ---
+
+Now, you need to pass through the firewall without being caught - easier said than done.
+
+You can't control the speed of the packet, but you can delay it any number of picoseconds. For each picosecond you delay the packet before beginning your trip, all security scanners move one step. You're not in the firewall during this time; you don't enter layer 0 until you stop delaying the packet.
+
+In the example above, if you delay 10 picoseconds (picoseconds 0 - 9), you won't get caught:
+
+State after delaying:
+ 0   1   2   3   4   5   6
+[ ] [S] ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+                [ ]     [ ]
+
+Picosecond 10:
+ 0   1   2   3   4   5   6
+( ) [S] ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+                [ ]     [ ]
+
+ 0   1   2   3   4   5   6
+( ) [ ] ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+
+
+Picosecond 11:
+ 0   1   2   3   4   5   6
+[ ] ( ) ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+
+ 0   1   2   3   4   5   6
+[S] (S) ... ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+
+
+Picosecond 12:
+ 0   1   2   3   4   5   6
+[S] [S] (.) ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+
+ 0   1   2   3   4   5   6
+[ ] [ ] (.) ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+
+
+Picosecond 13:
+ 0   1   2   3   4   5   6
+[ ] [ ] ... (.) [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+
+ 0   1   2   3   4   5   6
+[ ] [S] ... (.) [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+                [ ]     [ ]
+
+
+Picosecond 14:
+ 0   1   2   3   4   5   6
+[ ] [S] ... ... ( ) ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+                [ ]     [ ]
+
+ 0   1   2   3   4   5   6
+[ ] [ ] ... ... ( ) ... [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+                [S]     [S]
+
+
+Picosecond 15:
+ 0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] (.) [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+                [S]     [S]
+
+ 0   1   2   3   4   5   6
+[S] [S] ... ... [ ] (.) [ ]
+[ ] [ ]         [ ]     [ ]
+[ ]             [S]     [S]
+                [ ]     [ ]
+
+
+Picosecond 16:
+ 0   1   2   3   4   5   6
+[S] [S] ... ... [ ] ... ( )
+[ ] [ ]         [ ]     [ ]
+[ ]             [S]     [S]
+                [ ]     [ ]
+
+ 0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] ... ( )
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+                [ ]     [ ]
+Because all smaller delays would get you caught, the fewest number of picoseconds you would need to delay to get through safely is 10.
+
+What is the fewest number of picoseconds that you need to delay the packet to pass through the firewall without being caught?
  */
 
 const Utils = require('./utils.js');
@@ -197,4 +312,45 @@ function runPart1() {
     console.log(part1('0: 3\n1: 2\n2: 5\n4: 4\n6: 6\n8: 4\n10: 8\n12: 8\n14: 6\n16: 8\n18: 6\n20: 6\n22: 8\n24: 12\n26: 12\n28: 8\n30: 12\n32: 12\n34: 8\n36: 10\n38: 9\n40: 12\n42: 10\n44: 12\n46: 14\n48: 14\n50: 12\n52: 14\n56: 12\n58: 12\n60: 14\n62: 14\n64: 12\n66: 14\n68: 14\n70: 14\n74: 24\n76: 14\n80: 18\n82: 14\n84: 14\n90: 14\n94: 17'));
 }
 
+function part2(input) {
+    const regex = /(\d+): (\d+)/;
+
+    let delay = -1;
+    let validSolution = false;
+    let lines = input.split('\n');
+
+    while (!validSolution) {
+        validSolution = true;
+        delay++;
+
+        for (let i in lines) {
+            let match = regex.exec(lines[i]);
+
+            if (!match) {
+                throw Error('Invalid line format: ' + lines[i]);
+            }
+
+            let depth = parseInt(match[1]);
+            let range = parseInt(match[2]);
+
+            let curPos = (depth + delay) % (2 * (range - 1));
+
+            if (curPos === 0) {
+                validSolution = false;
+                break;
+            }
+        }
+    }
+
+    return delay;
+}
+
+function runPart2() {
+    Utils.assertAreEqual(10, part2('0: 3\n1: 2\n4: 4\n6: 4'));
+
+    // Answer: 3903378
+    console.log(part2('0: 3\n1: 2\n2: 5\n4: 4\n6: 6\n8: 4\n10: 8\n12: 8\n14: 6\n16: 8\n18: 6\n20: 6\n22: 8\n24: 12\n26: 12\n28: 8\n30: 12\n32: 12\n34: 8\n36: 10\n38: 9\n40: 12\n42: 10\n44: 12\n46: 14\n48: 14\n50: 12\n52: 14\n56: 12\n58: 12\n60: 14\n62: 14\n64: 12\n66: 14\n68: 14\n70: 14\n74: 24\n76: 14\n80: 18\n82: 14\n84: 14\n90: 14\n94: 17'));
+}
+
 runPart1();
+runPart2();
