eb54283595d4234fff170b06860fd446ae9cf7be
Rob Keim <robkeim@gmail.com>
2017-12-25 00:07:40 +0700

    [AdventOfCode] 2017 Add Day 24 Part 2 solution

--- AdventOfCode/2017/day24.js
+++ AdventOfCode/2017/day24.js
@@ -38,6 +38,17 @@ With them, you could make the following valid bridges:
 Of these bridges, the strongest one is 0/1--10/1--9/10; it has a strength of 0+1 + 1+10 + 10+9 = 31.
 
 What is the strength of the strongest bridge you can make with the components you have available?
+
+--- Part Two ---
+The bridge you've built isn't long enough; you can't jump the rest of the way.
+
+In the example above, there are two longest bridges:
+- 0/2--2/2--2/3--3/4
+- 0/2--2/2--2/3--3/5
+
+Of them, the one which uses the 3/5 component is stronger; its strength is 0+2 + 2+2 + 2+3 + 3+5 = 19.
+
+What is the strength of the longest bridge you can make? If you can make multiple bridges of the longest length, pick the strongest one.
  */
 
 const Utils = require('./utils.js');
@@ -111,4 +122,74 @@ function runPart1() {
     console.log(part1('25/13\n4/43\n42/42\n39/40\n17/18\n30/7\n12/12\n32/28\n9/28\n1/1\n16/7\n47/43\n34/16\n39/36\n6/4\n3/2\n10/49\n46/50\n18/25\n2/23\n3/21\n5/24\n46/26\n50/19\n26/41\n1/50\n47/41\n39/50\n12/14\n11/19\n28/2\n38/47\n5/5\n38/34\n39/39\n17/34\n42/16\n32/23\n13/21\n28/6\n6/20\n1/30\n44/21\n11/28\n14/17\n33/33\n17/43\n31/13\n11/21\n31/39\n0/9\n13/50\n10/14\n16/10\n3/24\n7/0\n50/50'));
 }
 
+function part2(input) {
+    let regex = /(\d+)\/(\d+)/;
+
+    let components = input.split('\n').map(line =>
+    {
+        let match = regex.exec(line);
+
+        if (!match) {
+            throw Error('Invalid line format: ' + line);
+        }
+
+        return [parseInt(match[1]), parseInt(match[2])];
+    });
+
+    return buildBridge2(components, [])[1];
+}
+
+function buildBridge2(components, soFar) {
+    let maxScore = calcBridgeValue2(soFar);
+    let lastValue = soFar.length === 0 ? 0 : soFar[soFar.length - 1][1];
+
+    for (let i = 0; i < components.length; i++) {
+        let remainingComponents, newChain, newValue;
+
+        if (components[i][0] === lastValue) {
+            remainingComponents = components.slice(0);
+            remainingComponents.splice(i, 1);
+            newChain = soFar.slice(0);
+            newChain.push(components[i]);
+            newValue = buildBridge2(remainingComponents, newChain);
+
+            if (newValue[0] > maxScore[0] || (newValue[0] === maxScore[0] && newValue[1] > maxScore[1])) {
+                maxScore = newValue;
+            }
+        }
+
+        if (components[i][1] === lastValue) {
+            remainingComponents = components.slice(0);
+            remainingComponents.splice(i, 1);
+            newChain = soFar.slice(0);
+            newChain.push(components[i].reverse());
+            newValue = buildBridge2(remainingComponents, newChain);
+
+            if (newValue[0] > maxScore[0] || (newValue[0] === maxScore[0] && newValue[1] > maxScore[1])) {
+                maxScore = newValue;
+            }
+        }
+    }
+
+    return maxScore;
+}
+
+function calcBridgeValue2(bridge) {
+    let value = 0;
+
+    for (let i = 0; i < bridge.length; i++) {
+        value += (bridge[i][0] + bridge[i][1]);
+    }
+
+    return [bridge.length, value];
+}
+
+function runPart2() {
+    Utils.assertAreEqual(19, part2('0/2\n2/2\n2/3\n3/4\n3/5\n0/1\n10/1\n9/10'));
+
+    // Answer: 1841
+    console.log(part2('25/13\n4/43\n42/42\n39/40\n17/18\n30/7\n12/12\n32/28\n9/28\n1/1\n16/7\n47/43\n34/16\n39/36\n6/4\n3/2\n10/49\n46/50\n18/25\n2/23\n3/21\n5/24\n46/26\n50/19\n26/41\n1/50\n47/41\n39/50\n12/14\n11/19\n28/2\n38/47\n5/5\n38/34\n39/39\n17/34\n42/16\n32/23\n13/21\n28/6\n6/20\n1/30\n44/21\n11/28\n14/17\n33/33\n17/43\n31/13\n11/21\n31/39\n0/9\n13/50\n10/14\n16/10\n3/24\n7/0\n50/50'));
+}
+
 runPart1();
+runPart2();
