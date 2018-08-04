58ba0e1eb8e62daeb75eea7e625a8209485ece04
Rob Keim <robkeim@gmail.com>
2018-08-04 12:23:42 +0700

    [ExercismPython] Simplify bob solution

--- Exercism/python/bob/bob.py
+++ Exercism/python/bob/bob.py
@@ -1,12 +1,10 @@
 def hey(phrase):
     phrase = phrase.strip()
 
-    if len(phrase) == 0:
+    if not phrase:
         return "Fine. Be that way!"
 
-    letters = filter(lambda x: x.isalpha(), phrase)
-
-    if len(letters) > 0 and all(letter.isupper() for letter in letters):
+    if str.isupper(phrase):
         if phrase.endswith("?"):
             return "Calm down, I know what I'm doing!"
         else:
