51754710179ae90b03275bd54a6b5565284429ad
Rob Keim <robkeim@gmail.com>
2018-12-01 12:18:06 +0700

    [AdventOfCode] Day 01 - placed 256

---
+++ AdventOfCode/2018/AdventOfCode/Day01.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public static class Day01
+    {
+        public static int Part1(string input)
+        {
+            var result = 0;
+
+            foreach (var line in input.Split("\n".ToCharArray()))
+            {
+                var value = int.Parse(line.Substring(1));
+
+                if (line[0] == '+')
+                {
+                    result += value;
+                }
+                else
+                {
+                    result -= value;
+                }
+            }
+
+            return result;
+        }
+
+        public static int Part2(string input)
+        {
+            var result = 0;
+            var seen = new HashSet<int>();
+            var index = 0;
+            var lines = input.Split("\n".ToCharArray());
+
+            while (true)
+            {
+                var value = int.Parse(lines[index].Substring(1));
+
+                if (lines[index][0] == '+')
+                {
+                    result += value;
+                }
+                else
+                {
+                    result -= value;
+                }
+
+                if (seen.Contains(result))
+                {
+                    return result;
+                }
+
+                seen.Add(result);
+
+                index++;
+                index %= lines.Length;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/2018/AdventOfCodeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCodeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdventOfCodeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25ff3b4e-e9c7-411b-9650-a7a264f91cdd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
