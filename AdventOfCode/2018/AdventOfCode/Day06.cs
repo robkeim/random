c52124679282d6aebc5bddfbff1df2943ca3d580
Rob Keim <robkeim@gmail.com>
2018-12-07 23:25:53 +0700

    [AdventOfCode] Add Day06 Part1 solution

---
+++ AdventOfCode/2018/AdventOfCode/Day06.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day06
+    {
+        public static int Part1(string input)
+        {
+            var coords = new List<Tuple<int, int>>();
+
+            // Parse input
+            foreach (var line in input.Split("\n".ToCharArray()))
+            {
+                var split = line
+                    .Split(",".ToCharArray())
+                    .Select(int.Parse)
+                    .ToArray();
+
+                coords.Add(new Tuple<int, int>(split[0], split[1]));
+            }
+
+            // Find bounds
+            var minX = int.MaxValue;
+            var minY = int.MaxValue;
+            var maxX = int.MinValue;
+            var maxY = int.MinValue;
+
+            foreach (var coord in coords)
+            {
+                if (coord.Item1 < minX)
+                {
+                    minX = coord.Item1;
+                }
+                else if (coord.Item1 > maxX)
+                {
+                    maxX = coord.Item1;
+                }
+
+                if (coord.Item2 < minY)
+                {
+                    minY = coord.Item2;
+                }
+                else if (coord.Item2 > maxY)
+                {
+                    maxY = coord.Item2;
+                }
+            }
+
+            // Find closest at each point
+            var closestAtPoint = new Dictionary<string, string>();
+
+            for (int x = minX; x <= maxX; x++)
+            {
+                for (int y = minY; y <= maxY; y++)
+                {
+                    var closestDist = int.MaxValue;
+                    var closestValue = string.Empty;
+                    var hasTie = false;
+
+                    for (int i = 0; i < coords.Count; i++)
+                    {
+                        var dist = Math.Abs(coords[i].Item1 - x) + Math.Abs(coords[i].Item2 - y);
+
+                        if (dist < closestDist)
+                        {
+                            closestDist = dist;
+                            closestValue = $"{coords[i].Item1}_{coords[i].Item2}";
+                            hasTie = false;
+                        }
+                        else if (dist == closestDist)
+                        {
+                            hasTie = true;
+                        }
+                    }
+
+                    if (!hasTie)
+                    {
+                        closestAtPoint[$"{x}_{y}"] = closestValue;
+                    }
+                    else
+                    {
+                        closestAtPoint[$"{x}_{y}"] = ".";
+                    }
+
+                }
+            }
+
+            // Determine infinite edges
+            var edges = new HashSet<string>();
+            edges.Add(".");
+
+            for (int x = minX; x <= maxX; x++)
+            {
+                edges.Add(closestAtPoint[$"{x}_{minY}"]);
+                edges.Add(closestAtPoint[$"{x}_{maxY}"]);
+            }
+
+            for (int y = minY; y <= maxY; y++)
+            {
+                edges.Add(closestAtPoint[$"{minX}_{y}"]);
+                edges.Add(closestAtPoint[$"{maxX}_{y}"]);
+            }
+
+            // Find largest area
+            var areas = closestAtPoint
+                .GroupBy(kvp => kvp.Value)
+                .OrderByDescending(group => group.Count())
+                .ToArray();
+
+            var index = 0;
+
+            while (true)
+            {
+                var group = areas[index];
+                if (!edges.Contains(group.Key))
+                {
+                    return group.Count();
+                }
+
+                index++;
+            }
+        }
+
+        public static int Part2(string input)
+        {
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/2018/AdventOfCodeTests/Day06Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    [TestClass]
+    public class Day06Tests
+    {
+        const string sampleInput = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9";
+        const string myInput = "315, 342\n59, 106\n44, 207\n52, 81\n139, 207\n93, 135\n152, 187\n271, 47\n223, 342\n50, 255\n332, 68\n322, 64\n250, 72\n165, 209\n129, 350\n139, 118\n282, 129\n311, 264\n216, 246\n134, 42\n66, 151\n263, 199\n222, 169\n236, 212\n320, 178\n202, 288\n273, 190\n83, 153\n88, 156\n284, 305\n131, 90\n152, 88\n358, 346\n272, 248\n317, 122\n166, 179\n301, 307\n156, 128\n261, 290\n268, 312\n89, 53\n324, 173\n353, 177\n91, 69\n303, 164\n40, 221\n146, 344\n61, 314\n319, 224\n98, 143";
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(17, Day06.Part1(sampleInput));
+            Assert.AreEqual(4290, Day06.Part1(myInput));
+        }
+
+        //[TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(-1, Day06.Part2(sampleInput));
+            Assert.AreEqual(-1, Day06.Part2(myInput));
+        }
+    }
+}
