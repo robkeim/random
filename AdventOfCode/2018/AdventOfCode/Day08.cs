2efd5140fb00fed80c8a610d2710a4ed5fa015c9
Rob Keim <robkeim@gmail.com>
2018-12-09 12:04:48 +0700

    [AdventOfCode] Add Day08 Part1 solution

---
+++ AdventOfCode/2018/AdventOfCode/Day08.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day08
+    {
+        public static int Part1(string input)
+        {
+            var numbers = input
+                .Split(" ".ToCharArray())
+                .Select(int.Parse)
+                .ToList();
+
+            return CountChecksums(numbers);
+        }
+
+        private static int CountChecksums(List<int> numbers)
+        {
+            var numChildren = numbers.First();
+            numbers.RemoveAt(0);
+
+            var numMetadata = numbers.First();
+            numbers.RemoveAt(0);
+
+            var result = 0;
+
+            for (int i = 0; i < numChildren; i++)
+            {
+                result += CountChecksums(numbers);
+            }
+
+            for (int i = 0; i < numMetadata; i++)
+            {
+                result += numbers.First();
+                numbers.RemoveAt(0);
+            }
+
+            return result;
+        }
+
+        public static int Part2(string input)
+        {
+            return -1;
+        }
+    }
+}
