c1135f60a034696eceea8f786dee4b53feb61a4f
Rob Keim <robkeim@gmail.com>
2018-12-09 13:42:33 +0700

    [AdventOfCode] Add Day09 Part1 solution

---
+++ AdventOfCode/2018/AdventOfCode/Day09.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class Day09
+    {
+        public static int Part1(string input)
+        {
+            var regex = new Regex(@"(\d+) players; last marble is worth (\d+) points", RegexOptions.Compiled);
+
+            var match = regex.Match(input);
+            var numPlayers = int.Parse(match.Groups[1].ToString());
+            var lastMarble = int.Parse(match.Groups[2].ToString());
+
+            var curMarbleIndex = 0;
+            var curPlayerIndex = 0;
+            var marbles = new List<int>();
+            marbles.Insert(0, 0);
+            var scores = new int[numPlayers];
+
+            for (int curMarble = 1; curMarble <= lastMarble; curMarble++)
+            {
+                if (curMarble % 23 != 0)
+                {
+                    curMarbleIndex = (curMarbleIndex + 2) % marbles.Count;
+                    marbles.Insert(curMarbleIndex + 1, curMarble);
+                }
+                else
+                {
+                    scores[curPlayerIndex] += curMarble;
+                    curMarbleIndex = (curMarbleIndex - 6 + marbles.Count) % marbles.Count;
+                    scores[curPlayerIndex] += marbles[curMarbleIndex];
+                    marbles.RemoveAt(curMarbleIndex);
+                    curMarbleIndex--;
+                }
+
+                curPlayerIndex = (curPlayerIndex + 1) % numPlayers;
+            }
+
+            return scores
+                .OrderByDescending(s => s)
+                .First();
+        }
+
+        public static int Part2(string input)
+        {
+            return Part1(input);
+        }
+    }
+}
---
+++ AdventOfCode/2018/AdventOfCodeTests/Day09Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    [TestClass]
+    public class Day09Tests
+    {
+        const string sampleInput = "9 players; last marble is worth 25 points";
+        const string myInput = "459 players; last marble is worth 71320 points";
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(32, Day09.Part1(sampleInput));
+            // Additional samples
+            Assert.AreEqual(8317, Day09.Part1("10 players; last marble is worth 1618 points"));
+            Assert.AreEqual(146373, Day09.Part1("13 players; last marble is worth 7999 points"));
+            Assert.AreEqual(2764, Day09.Part1("17 players; last marble is worth 1104 points"));
+            Assert.AreEqual(54718, Day09.Part1("21 players; last marble is worth 6111 points"));
+            Assert.AreEqual(37305, Day09.Part1("30 players; last marble is worth 5807 points"));
+            Assert.AreEqual(375414, Day09.Part1(myInput));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(-1, Day09.Part2("459 players; last marble is worth 7132000 points"));
+        }
+    }
+}
