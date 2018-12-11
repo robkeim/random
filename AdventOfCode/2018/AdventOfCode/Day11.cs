7ac2531ff6b4559a9b9b57912dc513bcfcca4c42
Rob Keim <robkeim@gmail.com>
2018-12-11 16:52:08 +0700

    [AdventOfCode] Add Day11 Part1 solution

---
+++ AdventOfCode/2018/AdventOfCode/Day11.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode
+{
+    public static class Day11
+    {
+        public static string Part1(int serialNumber)
+        {
+            var grid = new int[301, 301];
+
+            for (int x = 1; x <= 300; x++)
+            {
+                for (int y = 1; y <= 300; y++)
+                {
+                    grid[x, y] = PowerLevel(x, y, serialNumber);
+                }
+            }
+
+            var maxPower = int.MinValue;
+            var coordinate = string.Empty;
+
+            for (int x = 1; x <= 298; x++)
+            {
+                for (int y = 1; y <= 298; y++)
+                {
+                    var power = 0;
+
+                    for (int dx = 0; dx < 3; dx++)
+                    {
+                        for (int dy = 0; dy < 3; dy++)
+                        {
+                            power += grid[x + dx, y + dy];
+                        }
+                    }
+
+                    if (power > maxPower)
+                    {
+                        maxPower = power;
+                        coordinate = $"{x},{y}";
+                    }
+                }
+            }
+
+            return coordinate;
+        }
+
+        public static int PowerLevel(int x, int y, int serialNumber)
+        {
+            var rackId = x + 10;
+            var result = rackId * y;
+            result += serialNumber;
+            result *= rackId;
+            result %= 1000;
+            result /= 100;
+            result -= 5;
+
+            return result;
+        }
+
+        public static int Part2(int serialNumber)
+        {
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/2018/AdventOfCodeTests/Day11Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    [TestClass]
+    public class Day11Tests
+    {
+        const int sampleInput = 18;
+        const int myInput = 9445;
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(4, Day11.PowerLevel(3, 5, 8));
+            Assert.AreEqual(-5, Day11.PowerLevel(122, 79, 57));
+            Assert.AreEqual(0, Day11.PowerLevel(217, 196, 39));
+            Assert.AreEqual(4, Day11.PowerLevel(101, 153, 71));
+
+            Assert.AreEqual("33,45", Day11.Part1(sampleInput));
+            Assert.AreEqual("21,61", Day11.Part1(42));
+            Assert.AreEqual("233,36", Day11.Part1(myInput));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(-1, Day11.Part2(sampleInput));
+            Assert.AreEqual(-1, Day11.Part2(myInput));
+        }
+    }
+}
