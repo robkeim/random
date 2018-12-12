0b03a007095306f278eb871119ed502ddb503fa0
Rob Keim <robkeim@gmail.com>
2018-12-12 22:15:31 +0700

    [AdventOfCode] Add Day12 Part1 solution

---
+++ AdventOfCode/2018/AdventOfCode/Day12.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public static class Day12
+    {
+        public static int Part1(string initialState, string inputRules)
+        {
+            const int numGenerations = 20;
+            var growsPlant = new HashSet<string>();
+
+            foreach (var line in inputRules.Split("\n".ToCharArray()))
+            {
+                var split = line.Split(" =>".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                if (split[1] == "#")
+                {
+                    growsPlant.Add(split[0]);
+                }
+            }
+
+            for (int generation = 0; generation < numGenerations; generation++)
+            {
+                var newState = new StringBuilder();
+                
+                initialState = $"....{initialState}....";
+
+                for (int i = 2; i < initialState.Length - 2; i++)
+                {
+                    var toReplace = initialState.Substring(i - 2, 5);
+                    newState.Append(growsPlant.Contains(toReplace) ? "#" : ".");
+                }
+
+                initialState = newState.ToString();
+            }
+
+            var result = 0;
+
+            for (int i = 0; i < initialState.Length; i++)
+            {
+                if (initialState[i] == '#')
+                {
+                    result += (i - 2 * numGenerations);
+                }
+            }
+
+            return result;
+        }
+
+        public static int Part2(string initialState, string inputRules)
+        {
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/2018/AdventOfCodeTests/Day12Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    [TestClass]
+    public class Day12Tests
+    {
+        const string sampleInput = "#..#.#..##......###...###";
+        const string sampleInputRules = "...## => #\n..#.. => #\n.#... => #\n.#.#. => #\n.#.## => #\n.##.. => #\n.#### => #\n#.#.# => #\n#.### => #\n##.#. => #\n##.## => #\n###.. => #\n###.# => #\n####. => #";
+        const string myInput = "#.#####.##.###...#...#.####..#..#.#....##.###.##...#####.#..##.#..##..#..#.#.#.#....#.####....#..#";
+        const string myInputRules = "#.#.. => .\n..### => .\n...## => .\n.#### => #\n.###. => #\n#.... => .\n#.#.# => .\n###.. => #\n#..#. => .\n##### => #\n.##.# => #\n.#... => .\n##.## => #\n#...# => #\n.#.## => .\n##..# => .\n..... => .\n.#.#. => #\n#.### => #\n....# => .\n...#. => #\n..#.# => #\n##... => #\n####. => #\n#..## => #\n##.#. => #\n###.# => .\n#.##. => .\n..#.. => #\n.#..# => .\n..##. => .\n.##.. => #";
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(325, Day12.Part1(sampleInput, sampleInputRules));
+            Assert.AreEqual(4200, Day12.Part1(myInput, myInputRules));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(-1, Day12.Part2(sampleInput, sampleInputRules));
+            Assert.AreEqual(-1, Day12.Part2(myInput, myInputRules));
+        }
+    }
+}
