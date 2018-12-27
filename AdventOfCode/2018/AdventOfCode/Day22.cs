776f58a2dc4455a6d139cc7397030b3808375185
Rob Keim <robkeim@gmail.com>
2018-12-27 00:21:29 -0500

    [AdventOfCode] Add Day22 Part1 solution

---
+++ AdventOfCode/2018/AdventOfCode/Day22.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public static class Day22
+    {
+        public static int Part1(string input)
+        {
+            var split = input.Split("\n".ToCharArray());
+            var depth = int.Parse(split[0]);
+            var targetX = int.Parse(split[1]);
+            var targetY = int.Parse(split[2]);
+
+            var erosionLevels = new Dictionary<string, int>();
+            var geologicIndexes = new Dictionary<string, int>();
+
+            for (int x = 0; x <= targetX; x++)
+            {
+                for (int y = 0; y <= targetY; y++)
+                {
+                    var coord = $"{x}_{y}";
+
+                    if ((x == 0 && y == 0) || (x == targetX && y == targetY))
+                    {
+                        geologicIndexes[coord] = 0;
+                    }
+                    else if (y == 0)
+                    {
+                        geologicIndexes[coord] = x * 16807;
+                    }
+                    else if (x == 0)
+                    {
+                        geologicIndexes[coord] = y * 48271;
+                    }
+                    else
+                    {
+                        geologicIndexes[coord] = erosionLevels[$"{x - 1}_{y}"] * erosionLevels[$"{x}_{y - 1}"];
+                    }
+
+                    erosionLevels[coord] = (geologicIndexes[coord] + depth) % 20183;
+                }
+            }
+
+            var result = 0;
+
+            for (int x = 0; x <= targetX; x++)
+            {
+                for (int y = 0; y <= targetY; y++)
+                {
+                    result += erosionLevels[$"{x}_{y}"] % 3;
+                }
+            }
+
+            return result;
+        }
+
+        public static int Part2(string input)
+        {
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/2018/AdventOfCodeTests/Day22Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    [TestClass]
+    public class Day22Tests
+    {
+        const string sampleInput = "510\n10\n10";
+        const string myInput = "11991\n6\n797";
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(114, Day22.Part1(sampleInput));
+            Assert.AreEqual(5622, Day22.Part1(myInput));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(0, Day22.Part2(sampleInput));
+            Assert.AreEqual(0, Day22.Part2(myInput));
+        }
+    }
+}
