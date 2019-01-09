e58bfe045ba970cd91ebd7aaf3a61f852bc2bd27
Rob Keim <robkeim@gmail.com>
2019-01-09 18:59:32 +0700

    [AdventOfCode] Add Day25 Part1 solution for 2015

---
+++ AdventOfCode/2015/C#/AdventOfCode/Day25.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    public static class Day25
+    {
+        // Answer: 8997277
+        public static void Part1()
+        {
+            var maxRow = 1;
+            var curRow = 1;
+            var curCol = 1;
+            var result = 20151125L;
+            var targetRow = 3010;
+            var targetCol = 3019;
+
+            while (curRow != targetRow || curCol != targetCol)
+            {
+                while (curRow >= 1)
+                {
+                    if (curRow == targetRow && curCol == targetCol)
+                    {
+                        break;
+                    }
+
+                    result = (result * 252533) % 33554393;
+
+                    curRow--;
+                    curCol++;
+                }
+
+                if (curRow == targetRow && curCol == targetCol)
+                {
+                    break;
+                }
+
+                maxRow++;
+                curRow = maxRow;
+                curCol = 1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AdventOfCode/2015/C#/AdventOfCode/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Day25.Part1();
+
+            Console.WriteLine("done!");
+            Console.ReadLine();
+        }
+    }
+}
