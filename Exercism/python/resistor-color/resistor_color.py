1167730d88460d4f34c1a3c18ab7c4f993568516
Rob Keim <robkeim@gmail.com>
2019-07-11 21:35:11 +0200

    [Exercism] Add resistor color solution in python

---
+++ Exercism/python/resistor-color/resistor_color.py
@@ -0,0 +1,19 @@
+color_mapping = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+def color_code(color):
+    return color_mapping[color]
+
+
+def colors():
+    return [*color_mapping]
