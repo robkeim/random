9aaa7aa23a31eb4a2340b32e9b50ffcadbe32a1b
Rob Keim <robkeim@gmail.com>
2019-07-13 00:04:11 +0200

    [Exercism] Add rational numbers solution in python

---
+++ Exercism/python/rational-numbers/rational_numbers.py
@@ -0,0 +1,40 @@
+from __future__ import division
+
+class Rational(object):
+    def __init__(self, numer, denom):
+        gcd = calculate_gcd(numer, denom)
+        self.numer = numer / gcd
+        self.denom = denom / gcd
+
+    def __eq__(self, other):
+        return self.numer == other.numer and self.denom == other.denom
+
+    def __repr__(self):
+        return '{}/{}'.format(self.numer, self.denom)
+
+    def __add__(self, other):
+        return Rational(self.numer * other.denom + other.numer * self.denom, self.denom * other.denom)
+
+    def __sub__(self, other):
+        return Rational(self.numer * other.denom - other.numer * self.denom, self.denom * other.denom)
+
+    def __mul__(self, other):
+        return Rational(self.numer * other.numer, self.denom * other.denom)
+
+    def __truediv__(self, other):
+        return Rational(self.numer * other.denom, other.numer * self.denom)
+
+    def __abs__(self):
+        return Rational(abs(self.numer), abs(self.denom))
+
+    def __pow__(self, power):
+        return Rational(self.numer ** power, self.denom ** power)
+
+    def __rpow__(self, base):
+        return base ** (self.numer / float(self.denom))
+
+def calculate_gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return calculate_gcd(b, a % b)
