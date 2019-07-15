3e8f7efb0188063124e58dc01c8e55fb335c8b7d
Rob Keim <robkeim@gmail.com>
2019-07-15 21:36:23 +0200

    [Exercism] Add raindrops solution in python

---
+++ Exercism/python/raindrops/raindrops.py
@@ -0,0 +1,16 @@
+def convert(number):
+    result = ""
+
+    if number % 3 == 0:
+        result += "Pling"
+
+    if number % 5 == 0:
+        result += "Plang"
+
+    if number % 7 == 0:
+        result += "Plong"
+
+    if len(result) > 0:
+        return result
+    else:
+        return str(number)
