e0492d5d6617bb263443497277eb4ca997173363
Rob Keim <robkeim@gmail.com>
2019-08-03 01:35:40 +0200

    [Exercism] Simplify matrix solution in python

--- Exercism/python/matrix/matrix.py
+++ Exercism/python/matrix/matrix.py
@@ -1,19 +1,10 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.rows = []
-
-        for row in matrix_string.splitlines():
-            self.rows.append(list(map(int, row.split())))
-
-        self.cols = []
-
-        for i in range(0, len(self.rows[0])):
-            self.cols.append([])
-            for j in range(0, len(self.rows)):
-                self.cols[i].append(self.rows[j][i])
+        self.rows = [list(map(int, row.split())) for row in matrix_string.splitlines()]
+        self.cols = [[self.rows[j][i] for j in range(0, len(self.rows))] for i in range(0, len(self.rows[0]))]
 
     def row(self, index):
-        return self.rows[index - 1]
+        return list(self.rows[index - 1])
 
     def column(self, index):
-        return self.cols[index - 1]
+        return list(self.cols[index - 1])
