e0d05f41011bbb7ddd282ad8a25fe3b8c1981957
Rob Keim <robkeim@gmail.com>
2019-08-03 01:36:01 +0200

    [Exercism] Simplify hamming solution in python

--- Exercism/python/hamming/hamming.py
+++ Exercism/python/hamming/hamming.py
@@ -2,10 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands are unequal length")
 
-    result = 0
-
-    for i in range(0, len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            result += 1
-
-    return result
+    return len(list(filter(lambda x: x[0] != x[1], zip(strand_a, strand_b))))
