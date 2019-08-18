75b62fc376951df498bb8b46e851ef1c3051ff6a
Rob Keim <robkeim@gmail.com>
2019-08-18 16:23:00 +0200

    [Exercism] Simplify raindrops solution in python

--- Exercism/python/raindrops/raindrops.py
+++ Exercism/python/raindrops/raindrops.py
@@ -1,6 +1,6 @@
 def convert(number):
     sounds = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
 
-    result = "".join([x[1] for x in sounds if number % x[0] == 0])
+    result = "".join([sound for num, sound in sounds if number % num == 0])
 
     return result if len(result) > 0 else str(number)
