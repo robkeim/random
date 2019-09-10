8da0e31cd2fdd39bbd9c0a1cc65d65d619fc75c1
Rob Keim <robkeim@gmail.com>
2019-09-10 22:06:51 +0200

    [Exercism] Simplify clock solution in python

--- Exercism/python/clock/clock.py
+++ Exercism/python/clock/clock.py
@@ -1,15 +1,6 @@
 class Clock(object):
     def __init__(self, hours, minutes):
-        hours += minutes // 60
-        minutes = minutes % 60
-
-        hours %= 24
-
-        if hours < 0:
-            hours += 24
-
-        self.hours = hours
-        self.minutes = minutes
+        self.hours, self.minutes = divmod((hours * 60 + minutes) % (24 * 60), 60)
 
     def __repr__(self):
         return "{:02d}:{:02d}".format(self.hours, self.minutes)
