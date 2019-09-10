1b8efd1a3886f71c4eb77430958bb73800b5755b
Rob Keim <robkeim@gmail.com>
2019-09-10 22:07:19 +0200

    [Exercism] Simplify tournament solution in python

--- Exercism/python/tournament/tournament.py
+++ Exercism/python/tournament/tournament.py
@@ -1,59 +1,46 @@
+from collections import defaultdict
+
+
 def tally(rows):
-    wins = {}
-    draws = {}
-    losses = {}
+    wins = defaultdict(int)
+    draws = defaultdict(int)
+    losses = defaultdict(int)
     teams = set()
 
     for row in rows:
-        parts = row.split(";")
-        teams.add(parts[0])
-        teams.add(parts[1])
+        team1, team2, result = row.split(";")
+        teams.add(team1)
+        teams.add(team2)
 
-        if parts[2] == "win":
-            increment_value(wins, parts[0])
-            increment_value(losses, parts[1])
-        elif parts[2] == "draw":
-            increment_value(draws, parts[0])
-            increment_value(draws, parts[1])
-        elif parts[2] == "loss":
-            increment_value(losses, parts[0])
-            increment_value(wins, parts[1])
+        if result == "win":
+            wins[team1] += 1
+            losses[team2] += 1
+        elif result == "draw":
+            draws[team1] += 1
+            draws[team2] += 1
+        elif result == "loss":
+            losses[team1] += 1
+            wins[team2] += 1
         else:
             raise ValueError("Invalid result" + row)
 
     results = []
 
     for team in sorted(teams):
-        team_wins = get_value(wins, team)
-        team_draws = get_value(draws, team)
-        team_losses = get_value(losses, team)
+        team_wins = wins[team]
+        team_draws = draws[team]
+        team_losses = losses[team]
         matches_played = team_wins + team_draws + team_losses
         points = 3 * team_wins + team_draws
         formatted_line = "{:31}|  {} |  {} |  {} |  {} |  {}" \
             .format(team, matches_played, team_wins, team_draws, team_losses, points)
 
-        results.append((points, team, formatted_line))
-
-    results = list(map(lambda x: x[2], sorted(results, key=lambda x: x[0], reverse=True)))
+        results.append((points, formatted_line))
 
     result = [
         "Team                           | MP |  W |  D |  L |  P"
     ]
 
-    result.extend(results)
+    result.extend([x[1] for x in sorted(results, key=lambda x: x[0], reverse=True)])
 
     return result
-
-
-def increment_value(dictionary, value):
-    if value in dictionary:
-        dictionary[value] += 1
-    else:
-        dictionary[value] = 1
-
-
-def get_value(dictionary, value):
-    if value in dictionary:
-        return dictionary[value]
-
-    return 0
