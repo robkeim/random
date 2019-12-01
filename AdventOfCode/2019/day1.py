c84e25818f9556ab2badcdc8a4ca22ce741c91f0
Rob Keim <robkeim@gmail.com>
2019-12-01 12:34:28 +0100

    [AdventOfCode] Add 2019 day 1 solution

---
+++ AdventOfCode/2019/day1.py
@@ -0,0 +1,31 @@
+def part1():
+    lines = open("day1.txt").read().split()
+    print(sum([get_fuel(int(mass)) for mass in lines]))
+
+
+def part2():
+    lines = open("day1.txt").read().split()
+    print(sum([get_total_fuel(int(mass)) for mass in lines]))
+
+
+def get_total_fuel(amount):
+    total = 0
+
+    while amount > 0:
+        amount = get_fuel(amount)
+        total += amount
+
+    return total
+
+
+def get_fuel(amount):
+    return max(0, amount // 3 - 2)
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
