e1ec21622b572c32ac22aae1c979a96d083ee49e
Rob Keim <robkeim@gmail.com>
2019-12-02 23:39:27 +0100

    [AdventOfCode] Cleanup 2019 day 2 solution

--- AdventOfCode/2019/day2.py
+++ AdventOfCode/2019/day2.py
@@ -1,74 +1,63 @@
+OPCODE_ADD = 1
+OPCODE_MULTIPLY = 2
+OPCODE_QUIT = 99
+
+
 def part1():
-    data = [int(num) for num in open("day2.txt").read().split(",")]
-    op_index = 0
+    memory = [int(num) for num in open("day2.txt").read().split(",")]
 
-    data[1] = 12
-    data[2] = 2
+    memory[1] = 12
+    memory[2] = 2
+    run_iteration(memory)
 
-    while True:
-        op = data[op_index]
-
-        if op != 1 and op != 2:
-            break
-
-        val1 = data[data[op_index + 1]]
-        val2 = data[data[op_index + 2]]
-
-        if op == 1:
-            result = val1 + val2
-        else:
-            result = val1 * val2
-
-        data[data[op_index + 3]] = result
-
-        op_index += 4
-
-    print(data[0])
+    print(memory[0])
 
 
 def part2():
-    orig_data = [int(num) for num in open("day2.txt").read().split(",")]
-
-    found = False
+    orig_memory = [int(num) for num in open("day2.txt").read().split(",")]
 
     # Brute force through a range of possibilities
     for noun in range(0, 1000):
-        if found:
-            break
-
         for verb in range(0, 1000):
-            data = orig_data[:]
-            instruction_pointer = 0
+            memory = orig_memory[:]
 
-            data[1] = noun
-            data[2] = verb
+            memory[1] = noun
+            memory[2] = verb
 
             try:
-                while True:
-                    opcode = data[instruction_pointer]
+                run_iteration(memory)
 
-                    if opcode != 1 and opcode != 2:
-                        break
-
-                    val1 = data[data[instruction_pointer + 1]]
-                    val2 = data[data[instruction_pointer + 2]]
-
-                    if opcode == 1:
-                        result = val1 + val2
-                    else:
-                        result = val1 * val2
-
-                    data[data[instruction_pointer + 3]] = result
-
-                    instruction_pointer += 4
-
-                if data[0] == 19690720:
+                if memory[0] == 19690720:
                     print(str(100 * noun + verb))
-                    found = True
+                    return
             except Exception:
                 pass # Some iterations don't terminate and run out of instructions
 
         noun += 1
+
+
+def run_iteration(memory):
+    instruction_pointer = 0
+
+    while True:
+        opcode = memory[instruction_pointer]
+
+        if opcode == OPCODE_QUIT:
+            return
+
+        val1 = memory[memory[instruction_pointer + 1]]
+        val2 = memory[memory[instruction_pointer + 2]]
+
+        if opcode == OPCODE_ADD:
+            result = val1 + val2
+        elif opcode == OPCODE_MULTIPLY:
+            result = val1 * val2
+        else:
+            raise Exception("Unknown opcode: " + str(opcode))
+
+        memory[memory[instruction_pointer + 3]] = result
+
+        instruction_pointer += 4
 
 
 def main():
