cba436b2799542c261b64b4f5a59835f11865c71
Rob Keim <robkeim@gmail.com>
2019-12-02 20:18:49 +0100

    [AdventOfCode] Add 2019 day 2 part 2 solution

--- AdventOfCode/2019/day2.py
+++ AdventOfCode/2019/day2.py
@@ -21,13 +21,59 @@ def part1():
 
         data[data[op_index + 3]] = result
 
-        op_index = op_index + 4 % len(data)
+        op_index += 4
 
     print(data[0])
 
 
+def part2():
+    orig_data = [int(num) for num in open("day2.txt").read().split(",")]
+
+    found = False
+
+    # Brute force through a range of possibilities
+    for noun in range(0, 1000):
+        if found:
+            break
+
+        for verb in range(0, 1000):
+            data = orig_data[:]
+            instruction_pointer = 0
+
+            data[1] = noun
+            data[2] = verb
+
+            try:
+                while True:
+                    opcode = data[instruction_pointer]
+
+                    if opcode != 1 and opcode != 2:
+                        break
+
+                    val1 = data[data[instruction_pointer + 1]]
+                    val2 = data[data[instruction_pointer + 2]]
+
+                    if opcode == 1:
+                        result = val1 + val2
+                    else:
+                        result = val1 * val2
+
+                    data[data[instruction_pointer + 3]] = result
+
+                    instruction_pointer += 4
+
+                if data[0] == 19690720:
+                    print(str(100 * noun + verb))
+                    found = True
+            except Exception:
+                pass # Some iterations don't terminate and run out of instructions
+
+        noun += 1
+
+
 def main():
     part1()
+    part2()
 
 
 if __name__ == "__main__":
