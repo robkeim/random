e3a79bd81f468a3b9d4cc45112823ac4b444110d
Rob Keim <robkeim@gmail.com>
2019-12-06 17:48:54 +0100

    [AdventOfCode] 2015 Add day 19 part 2 solution

---
+++ AdventOfCode/2015/day19.py
@@ -0,0 +1,24 @@
+# This is for part 2 only
+
+from random import randint
+
+inputs = [line.strip().split(" => ")[::-1] for line in open("day19.txt").readlines()]
+inputs = [(value[0], value[1]) for value in inputs]
+
+result = "ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF"
+replacements = 0
+
+while result != "e":
+    input, replace = inputs[randint(0, len(inputs) - 1)]
+
+    new_result = result.replace(input, replace, 1)
+
+    if result == new_result:
+        continue
+
+    result = new_result
+    replacements += 1
+
+print(replacements)
+
+pass
