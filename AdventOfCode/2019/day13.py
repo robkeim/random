86b401ef7abbeace8a27d4b68e70dbc33b44b567
Rob Keim <robkeim@gmail.com>
2019-12-14 00:35:41 +0100

    [AdventOfCode] 2019 Add day 13 part 2 solution

--- AdventOfCode/2019/day13.py
+++ AdventOfCode/2019/day13.py
@@ -29,6 +29,9 @@ class Intcode:
         self.__relative_base = 0
         self.__inputs = []
 
+    def set_memory_address(self, address, value):
+        self.__memory[address] = value
+
     def run(self, inputs):
         result = self.run_to_next_output(inputs)
 
@@ -123,6 +126,92 @@ class Intcode:
             else:
                 raise Exception("Unknown opcode: " + str(opcode))
 
+    def run_to_next_input(self, inputs):
+        self.__inputs += inputs
+        outputs = []
+        while True:
+            opcode = self.__memory[self.__instruction_pointer]
+            modes = opcode // 100
+            opcode %= 100
+
+            modes = [int(val) for val in list(str(modes))][::-1]
+            while len(modes) < 3:
+                modes.append(0)  # Ensure there are enough modes to avoid out of range issues
+
+            if opcode == self.__OPCODE_QUIT:
+                return outputs
+
+            if opcode == self.__OPCODE_ADD:
+                val1 = self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0])
+                val2 = self.__get_value(self.__instruction_pointer + 2, self.__relative_base, modes[1])
+                result = val1 + val2
+
+                self.__set_value(self.__instruction_pointer + 3, self.__relative_base, modes[2], result)
+                self.__instruction_pointer += 4
+
+            elif opcode == self.__OPCODE_MULTIPLY:
+                val1 = self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0])
+                val2 = self.__get_value(self.__instruction_pointer + 2, self.__relative_base, modes[1])
+                result = val1 * val2
+
+                self.__set_value(self.__instruction_pointer + 3, self.__relative_base, modes[2], result)
+                self.__instruction_pointer += 4
+
+            elif opcode == self.__OPCODE_INPUT:
+                if len(self.__inputs) == 0:
+                    return outputs
+
+                self.__set_value(self.__instruction_pointer + 1, self.__relative_base, modes[0], self.__inputs[0])
+                self.__inputs = self.__inputs[1:]
+                self.__instruction_pointer += 2
+
+            elif opcode == self.__OPCODE_OUTPUT:
+                outputs.append(self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0]))
+                self.__instruction_pointer += 2
+
+            elif opcode == self.__OPCODE_JUMP_IF_TRUE:
+                if self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0]) != 0:
+                    self.__instruction_pointer = self.__get_value(self.__instruction_pointer + 2, self.__relative_base, modes[1])
+                else:
+                    self.__instruction_pointer += 3
+
+            elif opcode == self.__OPCODE_JUMP_IF_FALSE:
+                if self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0]) == 0:
+                    self.__instruction_pointer = self.__get_value(self.__instruction_pointer + 2, self.__relative_base, modes[1])
+                else:
+                    self.__instruction_pointer += 3
+
+            elif opcode == self.__OPCODE_LESS_THAN:
+                val1 = self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0])
+                val2 = self.__get_value(self.__instruction_pointer + 2, self.__relative_base, modes[1])
+
+                if val1 < val2:
+                    value_to_store = 1
+                else:
+                    value_to_store = 0
+
+                self.__set_value(self.__instruction_pointer + 3, self.__relative_base, modes[2], value_to_store)
+                self.__instruction_pointer += 4
+
+            elif opcode == self.__OPCODE_EQUALS:
+                val1 = self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0])
+                val2 = self.__get_value(self.__instruction_pointer + 2, self.__relative_base, modes[1])
+
+                if val1 == val2:
+                    value_to_store = 1
+                else:
+                    value_to_store = 0
+
+                self.__set_value(self.__instruction_pointer + 3, self.__relative_base, modes[2], value_to_store)
+                self.__instruction_pointer += 4
+
+            elif opcode == self.__OPCODE_ADJUST_RELATIVE_BASE:
+                self.__relative_base += self.__get_value(self.__instruction_pointer + 1, self.__relative_base, modes[0])
+                self.__instruction_pointer += 2
+
+            else:
+                raise Exception("Unknown opcode: " + str(opcode))
+
     def __get_value(self, instruction_pointer, relative_base, mode):
         if mode == self.__MODE_POSITION:
             return self.__memory[self.__memory[instruction_pointer]]
@@ -165,7 +254,65 @@ def part1():
 
 
 def part2():
-    pass
+    intcode = Intcode("day13.txt")
+    intcode.set_memory_address(0, 2)
+
+    outputs = intcode.run_to_next_input([])
+
+    blocks = set()
+    paddle = (0, 0)
+    ball = (0, 0)
+    score = 0
+    max_x = 0
+    max_y = 0
+
+    for x, y, tile_id in chunks(outputs, 3):
+        if x > max_x:
+            max_x = x
+        if y > max_y:
+            max_y = y
+        if x == -1 and y == 0:
+            score = tile_id
+        elif tile_id == 0 and (x, y) in blocks:
+            blocks.remove((x, y))
+        elif tile_id == 2:
+            blocks.add((x, y))
+        elif tile_id == 3:
+            paddle = (x, y)
+        elif tile_id == 4:
+            ball = (x, y)
+
+    while len(blocks) > 0:
+        if paddle[0] > ball[0]:
+            joystick = -1
+        elif paddle[0] < ball[0]:
+            joystick = 1
+        else:
+            joystick = 0
+
+        outputs = intcode.run_to_next_input([joystick])
+
+        if outputs is None:
+            print(len(blocks))
+            return
+
+        for x, y, tile_id in chunks(outputs, 3):
+            if x == -1 and y == 0:
+                score = tile_id
+            elif tile_id == 0 and (x, y) in blocks:
+                blocks.remove((x, y))
+            elif tile_id == 2:
+                blocks.add((x, y))
+            elif tile_id == 3:
+                paddle = (x, y)
+            elif tile_id == 4:
+                ball = (x, y)
+
+    print(score)
+
+
+def chunks(elements, size):
+    return [elements[i * size:(i + 1) * size] for i in range((len(elements) + size - 1) // size)]
 
 
 def main():
