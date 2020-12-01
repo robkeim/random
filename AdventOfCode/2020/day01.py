67ed2c3fa3887cab8fbf4af5f7fbd0a98916651b
Rob Keim <robkeim@gmail.com>
2020-12-01 09:09:41 +0100

    [AdventOfCode] 2020 Add day 1 parts 1 and 2 solution

--- AdventOfCode/2020/day01.py
+++ AdventOfCode/2020/day01.py
@@ -1,9 +1,26 @@
 def part1():
-    pass
+    values = [int(value.strip()) for value in open("day01.txt").readlines()]
+    values_set = set(values)
+
+    for value in values:
+        if 2020 - value in values_set:
+            print(value * (2020 - value))
+            return
+
+    raise Exception("No solution found")
 
 
 def part2():
-    pass
+    values = [int(value.strip()) for value in open("day01.txt").readlines()]
+
+    for value1 in values:
+        for value2 in values:
+            for value3 in values:
+                if value1 + value2 + value3 == 2020:
+                    print(value1 * value2 * value3)
+                    return
+
+    raise Exception("No solution found")
 
 
 def main():
