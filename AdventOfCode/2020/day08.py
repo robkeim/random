5546acb45b979a2264b4e3fd00d7150c0250cd41
Rob Keim <robkeim@gmail.com>
2020-12-08 08:24:35 +0100

    [AdventOfCode] 2020 Add day 8 part 2 solution

--- AdventOfCode/2020/day08.py
+++ AdventOfCode/2020/day08.py
@@ -29,7 +29,54 @@ def part1():
 
 
 def part2():
-    pass
+    instructions = [line.strip().split(" ") for line in open("day08.txt").readlines()]
+
+    for instruction in instructions:
+        instruction[1] = int(instruction[1])
+
+    for i in range(len(instructions)):
+        if instructions[i][0] != "nop" and instructions[i][0] != "jmp":
+            continue
+
+        instructions_tmp = [instruction[:] for instruction in instructions[:]]
+        instructions_tmp[i][0] = "jmp" if instructions_tmp[i][0] == "nop" else "nop"
+
+        result = execute_program(instructions_tmp)
+
+        if result:
+            print(result)
+            return
+
+
+def execute_program(instructions):
+    instruction_pointer = 0
+    accumulator = 0
+    seen = set()
+
+    while instruction_pointer < len(instructions):
+        if instruction_pointer in seen:
+            return None
+
+        seen.add(instruction_pointer)
+
+        operation, value = instructions[instruction_pointer]
+
+        if operation == "jmp":
+            instruction_pointer += value
+        else:
+            instruction_pointer += 1
+
+            if operation == "nop":
+                pass
+            elif operation == "acc":
+                accumulator += value
+            else:
+                raise Exception("Invalid operation: " + operation)
+
+    if instruction_pointer == len(instructions):
+        return accumulator
+
+    return None
 
 
 def main():
