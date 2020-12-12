4ecfecaf0dc36fb8db50585f084d5a42e9ede9e2
Rob Keim <robkeim@gmail.com>
2020-12-12 11:37:50 +0100

    [AdventOfCode] 2020 Add day 12 part 2 solution

--- AdventOfCode/2020/day12.py
+++ AdventOfCode/2020/day12.py
@@ -27,7 +27,49 @@ def part1():
 
 
 def part2():
-    pass
+    lines = [(line[0], int(line.strip()[1:])) for line in open("day12.txt").readlines()]
+    ship_x = ship_y = 0
+    waypoint_x = 10
+    waypoint_y = 1
+
+    for instruction, value in lines:
+        if instruction == "N":
+            waypoint_y += value
+        elif instruction == "S":
+            waypoint_y -= value
+        elif instruction == "E":
+            waypoint_x += value
+        elif instruction == "W":
+            waypoint_x -= value
+        elif instruction == "L" or instruction == "R":
+            if instruction == "L":
+                value = ((value * -1) + 360) % 360
+
+            delta_x = ship_x - waypoint_x
+            delta_y = ship_y - waypoint_y
+
+            if value == 90:
+                waypoint_x = ship_x - delta_y
+                waypoint_y = ship_y + delta_x
+            elif value == 180:
+                waypoint_x = ship_x + delta_x
+                waypoint_y = ship_y + delta_y
+            elif value == 270:
+                waypoint_x = ship_x + delta_y
+                waypoint_y = ship_y - delta_x
+            else:
+                raise Exception("Invalid rotation: " + str(value))
+        elif instruction == "F":
+            delta_x = (waypoint_x - ship_x) * value
+            delta_y = (waypoint_y - ship_y) * value
+            ship_x += delta_x
+            ship_y += delta_y
+            waypoint_x += delta_x
+            waypoint_y += delta_y
+        else:
+            raise Exception("Invalid instruction: " + instruction)
+
+    print(abs(ship_x) + abs(ship_y))
 
 
 def main():
