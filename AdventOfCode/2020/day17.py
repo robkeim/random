3feb40c351a63bacafe13a1fb2ed0ee011a67037
Rob Keim <robert.keim@blablacar.com>
2020-12-17 09:20:13 +0100

    [AdventOfCode] 2020 Add day 17 part 1 solution

---
+++ AdventOfCode/2020/day17.py
@@ -0,0 +1,67 @@
+def part1():
+    grid = [line.strip() for line in open("day17.txt").readlines()]
+
+    min_x = min_y = float("inf")
+    max_x = max_y = float("-inf")
+    min_z = max_z = 0
+    state = set()
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == "#":
+                state.add((x, y, 0))
+                min_x = min(min_x, x)
+                min_y = min(min_y, y)
+                max_x = max(max_x, x)
+                max_y = max(max_y, y)
+
+    for cycle in range(6):
+        min_x_next = min_y_next = min_z_next = float("inf")
+        max_x_next = max_y_next = max_z_next = float("-inf")
+        state_next = set()
+
+        for x in range(min_x - 1, max_x + 2):
+            for y in range(min_y - 1, max_y + 2):
+                for z in range(min_z - 1, max_z + 2):
+                    neighbors = 0
+
+                    for dx in range(-1, 2):
+                        for dy in range(-1, 2):
+                            for dz in range(-1, 2):
+                                if dx == 0 and dy == 0 and dz == 0:
+                                    continue
+
+                                if (x + dx, y + dy, z + dz) in state:
+                                    neighbors += 1
+
+                    if ((x, y, z) in state and 2 <= neighbors <= 3) or ((x, y, z) not in state and neighbors == 3):
+                        state_next.add((x, y, z))
+                        min_x_next = min(min_x_next, x)
+                        min_y_next = min(min_y_next, y)
+                        min_z_next = min(min_z_next, z)
+                        max_x_next = max(max_x_next, x)
+                        max_y_next = max(max_y_next, y)
+                        max_z_next = max(max_z_next, z)
+
+        state = state_next
+        min_x = min_x_next
+        min_y = min_y_next
+        min_z = min_z_next
+        max_x = max_x_next
+        max_y = max_y_next
+        max_z = max_z_next
+
+    print(len(state))
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
