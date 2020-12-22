6e86ef0c23a922b6730a344b499d07458e57cfb5
Rob Keim <robert.keim@blablacar.com>
2020-12-22 22:44:01 +0100

    [AdventOfCode] 2020 Add day 22 part 2 solution

--- AdventOfCode/2020/day22.py
+++ AdventOfCode/2020/day22.py
@@ -1,4 +1,32 @@
 def part1():
+    p1, p2 = parse_input()
+
+    while len(p1) > 0 and len(p2) > 0:
+        p1_turn = p1.pop(0)
+        p2_turn = p2.pop(0)
+
+        if p1_turn > p2_turn:
+            p1.append(p1_turn)
+            p1.append(p2_turn)
+        elif p2_turn > p1_turn:
+            p2.append(p2_turn)
+            p2.append(p1_turn)
+        else:
+            assert False, "Deck shouldn't contain ties"
+
+    winner = p1 if len(p1) > 0 else p2
+
+    print(score_game(winner))
+
+
+def part2():
+    p1, p2 = parse_input()
+    _, cards = play_game(p1, p2)
+
+    print(score_game(cards))
+
+
+def parse_input():
     lines = [line.strip() for line in open("day22.txt").readlines()]
 
     p1 = []
@@ -20,31 +48,60 @@ def part1():
         else:
             p1.append(int(line))
 
+    return p1, p2
+
+
+def play_game(p1, p2):
+    seen = set()
     while len(p1) > 0 and len(p2) > 0:
+        hash_value = get_hash(p1, p2)
+
+        if hash_value in seen:
+            return "p1", p1
+
+        seen.add(hash_value)
+
         p1_turn = p1.pop(0)
         p2_turn = p2.pop(0)
 
-        if p1_turn > p2_turn:
-            p1.append(p1_turn)
-            p1.append(p2_turn)
-        elif p2_turn > p1_turn:
-            p2.append(p2_turn)
-            p2.append(p1_turn)
+        if p1_turn <= len(p1) and p2_turn <= len(p2):
+            winner, _ = play_game(p1[:p1_turn], p2[:p2_turn])
+
+            if winner == "p1":
+                p1.append(p1_turn)
+                p1.append(p2_turn)
+            elif winner == "p2":
+                p2.append(p2_turn)
+                p2.append(p1_turn)
+            else:
+                assert False, "Invalid winner: " + winner
         else:
-            assert False, "Deck shouldn't contain ties"
+            if p1_turn > p2_turn:
+                p1.append(p1_turn)
+                p1.append(p2_turn)
+            elif p2_turn > p1_turn:
+                p2.append(p2_turn)
+                p2.append(p1_turn)
+            else:
+                assert False, "Deck shouldn't contain ties"
 
-    winner = p1 if len(p1) > 0 else p2
+    if len(p1) > 0:
+        return "p1", p1
+    else:
+        return "p2", p2
 
+
+def get_hash(p1, p2):
+    return tuple(p1), tuple(p2)
+
+
+def score_game(cards):
     score = 0
 
-    for i, value in enumerate(winner[::-1]):
+    for i, value in enumerate(cards[::-1]):
         score += (i + 1) * value
 
-    print(score)
-
-
-def part2():
-    pass
+    return score
 
 
 def main():
