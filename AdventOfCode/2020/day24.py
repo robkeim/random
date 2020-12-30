7fd7b757571e391773271acec143220ed0277f43
Rob Keim <robert.keim@blablacar.com>
2020-12-30 23:08:38 +0100

    [AdventOfCode] 2020 Add day 24 part 1 solution

---
+++ AdventOfCode/2020/day24.py
@@ -0,0 +1,51 @@
+import re
+
+
+def part1():
+    lines = [line.strip() for line in open("day24.txt").readlines()]
+
+    black_tiles = set()
+
+    for directions in lines:
+        position = (0, 0, 0)
+
+        for direction in re.findall("nw|ne|e|se|sw|w", directions):
+            position = get_next_position(position, direction)
+
+        if position in black_tiles:
+            black_tiles.remove(position)
+        else:
+            black_tiles.add(position)
+
+    print(len(black_tiles))
+
+
+def part2():
+    pass
+
+
+# Use cube coordinates defined here: https://www.redblobgames.com/grids/hexagons
+def get_next_position(position, direction):
+    if direction == "nw":
+        return position[0], position[1] + 1, position[2] - 1
+    elif direction == "ne":
+        return position[0] + 1, position[1], position[2] - 1
+    elif direction == "e":
+        return position[0] + 1, position[1] - 1, position[2]
+    elif direction == "se":
+        return position[0], position[1] - 1, position[2] + 1
+    elif direction == "sw":
+        return position[0] - 1, position[1], position[2] + 1
+    elif direction == "w":
+        return position[0] - 1, position[1] + 1, position[2]
+    else:
+        assert False, "Invalid direction: " + direction
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
