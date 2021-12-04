ae52ab566dc84c106d47e6f6359bf3c7acb3571e
Rob Keim <robkeim@gmail.com>
2021-12-04 01:16:23 +0100

    [AdventOfCode] 2021 Add day 3 part 2 solution

--- AdventOfCode/2021/day03.py
+++ AdventOfCode/2021/day03.py
@@ -29,7 +29,45 @@ def part1():
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("day03.txt").readlines()]
+
+    oxygen_generator_rating = calculate_life_support_criteria(
+        lines,
+        lambda num_zeros, num_ones: "1" if num_ones >= num_zeros else "0"
+    )
+
+    co2_scrubber_rating = calculate_life_support_criteria(
+        lines,
+        lambda num_zeros, num_ones: "0" if num_zeros <= num_ones else "1"
+    )
+
+    print(oxygen_generator_rating * co2_scrubber_rating)
+
+
+def calculate_life_support_criteria(lines, to_keep_fn):
+    cur_bit = 0
+
+    while len(lines) > 1:
+        num_zeros, num_ones = get_counts_in_column(lines, cur_bit)
+        to_keep = to_keep_fn(num_zeros, num_ones)
+
+        lines = [line for line in lines if line[cur_bit] == to_keep]
+
+        cur_bit += 1
+
+    return int(lines[0], 2)
+
+
+def get_counts_in_column(lines, column):
+    num_zeros = num_ones = 0
+
+    for line in lines:
+        if line[column] == "0":
+            num_zeros += 1
+        else:
+            num_ones += 1
+
+    return num_zeros, num_ones
 
 
 def main():
