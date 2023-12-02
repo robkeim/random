9731d513262559765e41261b1bc54ec58298972f
Rob Keim <robkeim@gmail.com>
2023-12-02 06:44:22 +0100

    [AdventOfCode] 2023 Cleanup day 1 parts 1 and 2

--- AdventOfCode/2023/day01.py
+++ AdventOfCode/2023/day01.py
@@ -1,66 +1,54 @@
+import re
+
+
 def part1():
     lines = [line.strip() for line in open("day01.txt").readlines()]
-    digit_set = set("0123456789")
 
     result = 0
 
     for line in lines:
-        digits = [letter for letter in line if letter in digit_set]
-        result += int(digits[0] + digits[len(digits) - 1])
+        matches = re.findall("([0-9])", line)
+        result += int(matches[0] + matches[len(matches) - 1])
 
     print(result)
 
 
 def part2():
     lines = [line.strip() for line in open("day01.txt").readlines()]
-    digit_set = set("0123456789")
 
-    replacements = [
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
-    ]
+    replacements = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9"
+    }
+    regex = "([0-9]|one|two|three|four|five|six|seven|eight|nine)"
 
     result = 0
 
     for line in lines:
-        first_digit = None
-        last_digit = None
+        first_digit = replacements[re.search(regex, line)[0]]
 
-        for i in range(0, len(line)):
-            if first_digit:
+        for i in range(len(line) - 1, -1, -1):
+            match = re.search(regex, line[i:])
+
+            if match:
+                last_digit = replacements[match[0]]
                 break
-
-            if line[i] in digit_set:
-                first_digit = line[i]
-                break
-
-            for original, replacement in replacements:
-                if line[i:].startswith(original):
-                    first_digit = replacement
-                    break
-
-        for i in range(len(line) - 1, 0, -1):
-            if last_digit:
-                break
-
-            if line[i] in digit_set:
-                last_digit = line[i]
-                break
-
-            for original, replacement in replacements:
-                if line[i:].startswith(original):
-                    last_digit = replacement
-                    break
-
-        if not last_digit:
-            last_digit = first_digit
 
         result += int(first_digit + last_digit)
 
