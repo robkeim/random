46b0518b796f1cbb56288c88e72405d892a1882e
Rob Keim <robkeim@gmail.com>
2023-12-03 14:29:15 +0100

    [AdventOfCode] 2023 Add day 3 part 1 solution

---
+++ AdventOfCode/2023/day03.py
@@ -0,0 +1,174 @@
+def part1():
+    grid = [line.strip() for line in open("day03.txt").readlines()]
+    print(part1logic(grid))
+
+
+def part1logic(grid):
+    width = len(grid[0])
+    height = len(grid)
+    digits = set("0123456789")
+    symbols = set()
+    numbers = []
+
+    # Extract numbers and symbols
+    for y in range(height):
+        num = None
+        for x in range(width):
+            char = grid[y][x]
+
+            if char in digits:
+                if num:
+                    num += char
+                else:
+                    num = char
+
+                continue
+
+            if char != ".":
+                symbols.add("{},{}".format(x, y))
+
+            if num:
+                start_x = x - len(num)
+                numbers.append((start_x, y, num))
+                num = None
+
+        if num:
+            start_x = width - len(num)
+            numbers.append((start_x, y, num))
+
+    result = 0
+
+    # Find non-adjacent numbers
+    for start_x, start_y, number in numbers:
+        has_adjacent = False
+
+        for x in range(start_x - 1, start_x + len(number) + 1): # Is this +1 or 2 right?
+            if has_adjacent:
+                break
+
+            for y in range(start_y - 1, start_y + 2):
+                if "{},{}".format(x, y) in symbols:
+                    has_adjacent = True
+                    result += int(number)
+                    break
+
+    return result
+
+
+def part1tests():
+    test_cases = [
+        ([
+            "1..",
+            "...",
+            "..."
+        ], 1),
+        ([
+             ".1.",
+             "...",
+             "..."
+         ], 1),
+        ([
+             "..1",
+             "...",
+             "..."
+         ], 1),
+        ([
+             "...",
+             "1..",
+             "..."
+         ], 1),
+        ([
+             "...",
+             ".1.",
+             "..."
+         ], 1),
+        ([
+             "...",
+             "..1",
+             "..."
+         ], 1),
+        ([
+             "...",
+             "...",
+             "1.."
+         ], 1),
+        ([
+             "...",
+             "...",
+             ".1."
+         ], 1),
+        ([
+             "...",
+             "...",
+             "..1"
+         ], 1),
+        ([
+             "1..",
+             ".*.",
+             "..."
+         ], 0),
+        ([
+             ".1.",
+             ".*.",
+             "..."
+         ], 0),
+        ([
+             "..1",
+             ".*.",
+             "..."
+         ], 0),
+        ([
+             "...",
+             "1*.",
+             "..."
+         ], 0),
+        ([
+             "...",
+             ".*1",
+             "..."
+         ], 0),
+        ([
+             "...",
+             ".*.",
+             "1.."
+         ], 0),
+        ([
+             "...",
+             ".*.",
+             ".1."
+         ], 0),
+        ([
+             "...",
+             ".*.",
+             "..1"
+         ], 0),
+        ([
+             "12.",
+             "..*",
+             "..."
+         ], 0),
+        ([
+             ".123..",
+             ".....*",
+             "......"
+         ], 123),
+    ]
+
+    for grid, expected_result in test_cases:
+        result = part1logic(grid)
+
+        if expected_result != result:
+            raise Exception("Expected {}, Actual {}".format(expected_result, result))
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
