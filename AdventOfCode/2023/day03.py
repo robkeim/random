28a915b9b091d2cbde3d80c4bbf8c5a5713831ab
Rob Keim <robkeim@gmail.com>
2023-12-03 21:38:31 +0100

    [AdventOfCode] 2023 Add day 3 part 2 solution

--- AdventOfCode/2023/day03.py
+++ AdventOfCode/2023/day03.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def part1():
     grid = [line.strip() for line in open("day03.txt").readlines()]
     print(part1logic(grid))
@@ -162,7 +165,65 @@ def part1tests():
 
 
 def part2():
-    pass
+    grid = [line.strip() for line in open("day03.txt").readlines()]
+
+    width = len(grid[0])
+    height = len(grid)
+    digits = set("0123456789")
+    stars = set()
+    numbers = []
+
+    # Extract numbers and symbols
+    for y in range(height):
+        num = None
+        for x in range(width):
+            char = grid[y][x]
+
+            if char in digits:
+                if num:
+                    num += char
+                else:
+                    num = char
+
+                continue
+
+            if char == "*":
+                stars.add("{},{}".format(x, y))
+
+            if num:
+                start_x = x - len(num)
+                numbers.append((start_x, y, num))
+                num = None
+
+        if num:
+            start_x = width - len(num)
+            numbers.append((start_x, y, num))
+
+    # Find gears
+    adjacents = defaultdict(set)
+
+    for start_x, start_y, number in numbers:
+        for x in range(start_x - 1, start_x + len(number) + 1): # Is this +1 or 2 right?
+            for y in range(start_y - 1, start_y + 2):
+                coordinate = "{},{}".format(x, y)
+
+                if coordinate in stars:
+                    adjacents[coordinate].add(number)
+
+    result = 0
+
+    for coordinate in adjacents:
+        adjacent_numbers = adjacents[coordinate]
+
+        if len(adjacent_numbers) == 2:
+            gear_ratio = 1
+
+            for adjacent_number in adjacent_numbers:
+                gear_ratio *= int(adjacent_number)
+
+            result += gear_ratio
+
+    print(result)
 
 
 def main():
