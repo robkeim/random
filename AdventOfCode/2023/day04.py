96f30e1c60910ea9805371420c610fc349a79896
Rob Keim <robkeim@gmail.com>
2023-12-04 23:05:55 +0100

    [AdventOfCode] 2023 Add day 4 part 2 solution

--- AdventOfCode/2023/day04.py
+++ AdventOfCode/2023/day04.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 def part1():
     lines = [line.strip() for line in open("day04.txt").readlines()]
 
@@ -13,8 +16,35 @@ def part1():
     print(result)
 
 
+cards_won = dict()
+
+
 def part2():
-    pass
+    lines = [line.strip() for line in open("day04.txt").readlines()]
+
+    for index, line in enumerate(lines):
+        winning_numbers, my_numbers = line.split(":")[1].split(" | ")
+        winning_numbers = set([number for number in winning_numbers.split()])
+        my_numbers = [number for number in my_numbers.split() if number in winning_numbers]
+
+        cards_won[index] = len(my_numbers)
+
+    result = 0
+
+    for i in range(len(lines)):
+        result += 1 + part2helper(i)
+
+    print(result)
+
+
+@lru_cache(maxsize=1000)
+def part2helper(index):
+    result = 0
+
+    for i in range(cards_won[index]):
+        result += 1 + part2helper(index + i + 1)
+
+    return result
 
 
 def main():
