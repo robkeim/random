f9434aab4e94c2da6e0dd8f0d428d5e009e3b79b
Rob Keim <robkeim@gmail.com>
2023-12-07 18:50:40 +0100

    [AdventOfCode] 2023 Simplify day 7 part 1 solution

--- AdventOfCode/2023/day07.py
+++ AdventOfCode/2023/day07.py
@@ -1,68 +1,53 @@
 from collections import Counter
 
+five_of_a_kind = 1
+four_of_a_kind = 2
+full_house = 3
+three_of_a_kind = 4
+two_pair = 5
+one_pair = 6
+high_card = 7
+
 
 def part1():
     lines = [line.strip() for line in open("day07.txt").readlines()]
-    lines = [(convert_to_hex(line.split()[0]), int(line.split()[1])) for line in lines]
-    print(lines)
-
-    five_of_a_kind = []
-    four_of_a_kind = []
-    full_house = []
-    three_of_a_kind = []
-    two_pair = []
-    one_pair = []
-    high_card = []
-
-    for hand, rank in lines:
-        values = Counter(hand)
-
-        if len(values) == 1:
-            five_of_a_kind.append((hand, rank))
-        elif len(values) == 2:
-            found = False
-            for key in values:
-                if values[key] == 4:
-                    four_of_a_kind.append((hand, rank))
-                    found = True
-                    break
-
-            if not found:
-                full_house.append((hand, rank))
-        elif len(values) == 3:
-            found = False
-            for key in values:
-                if values[key] == 3:
-                    three_of_a_kind.append((hand, rank))
-                    found = True
-                    break
-
-            if not found:
-                two_pair.append((hand, rank))
-        elif len(values) == 4:
-            one_pair.append((hand, rank))
-        else:
-            high_card.append((hand, rank))
+    lines = [(convert_to_hex(line.split()[0], False), int(line.split()[1])) for line in lines]
 
     sorted_hands = []
-    sorted_hands += sorted(high_card, key=lambda x: int(x[0], 16))
-    sorted_hands += sorted(one_pair, key=lambda x: int(x[0], 16))
-    sorted_hands += sorted(two_pair, key=lambda x: int(x[0], 16))
-    sorted_hands += sorted(three_of_a_kind, key=lambda x: int(x[0], 16))
-    sorted_hands += sorted(full_house, key=lambda x: int(x[0], 16))
-    sorted_hands += sorted(four_of_a_kind, key=lambda x: int(x[0], 16))
-    sorted_hands += sorted(five_of_a_kind, key=lambda x: int(x[0], 16))
+
+    for hand, rank in lines:
+        cards = Counter(hand)
+
+        if len(cards) == 1:
+            sorted_hands.append((five_of_a_kind, hand, rank))
+        elif len(cards) == 2:
+            if 4 in cards.values():
+                sorted_hands.append((four_of_a_kind, hand, rank))
+            else:
+                sorted_hands.append((full_house, hand, rank))
+        elif len(cards) == 3:
+            if 3 in cards.values():
+                sorted_hands.append((three_of_a_kind, hand, rank))
+            else:
+                sorted_hands.append((two_pair, hand, rank))
+        elif len(cards) == 4:
+            sorted_hands.append((one_pair, hand, rank))
+        else:
+            sorted_hands.append((high_card, hand, rank))
+
+    sorted_hands.sort(key=lambda x: (-x[0], int(x[1], 16)))
 
     result = 0
 
     for i in range(len(sorted_hands)):
-        result += sorted_hands[i][1] * (i + 1)
+        result += sorted_hands[i][2] * (i + 1)
 
     print(result)
 
 
-def convert_to_hex(card):
-    return card.replace("A", "E").replace("K", "D").replace("Q", "C").replace("J", "B").replace("T", "A")
+def convert_to_hex(card, has_jokers):
+    joker_replacement = "1" if has_jokers else "B"
+    return card.replace("A", "E").replace("K", "D").replace("Q", "C").replace("J", joker_replacement).replace("T", "A")
 
 
 def part2():
