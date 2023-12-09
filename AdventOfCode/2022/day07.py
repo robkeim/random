292c9f1826d492d12394f9e76bb8d0f840177f6c
Rob Keim <robkeim@gmail.com>
2023-12-09 22:12:36 +0100

    [AdventOfCode] 2022 Add day 7 part 1 solution

---
+++ AdventOfCode/2022/day07.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+
+
+# Make the assumption that we're only going to visit each directory once
+def part1():
+    lines = [line.strip() for line in open("day07.txt").readlines()]
+    index = 0
+    cur_dir = None
+    sizes = defaultdict(int)
+
+    while index < len(lines):
+        line = lines[index]
+
+        if line.startswith("$ cd"):
+            argument = line.split()[2]
+
+            if argument == "/":
+                cur_dir = []
+            elif argument == "..":
+                cur_dir.pop()
+            else:
+                cur_dir.append(argument)
+
+            index += 1
+        elif line == "$ ls":
+            index += 1
+
+            while index < len(lines) and not lines[index].startswith("$"):
+                if not lines[index].startswith("dir"):
+                    file_size = int(lines[index].split()[0])
+
+                    to_add = list(cur_dir)
+
+                    while len(to_add) > 0:
+                        sizes[tuple(to_add)] += file_size
+                        to_add.pop()
+
+                index += 1
+        else:
+            assert False, "Unexpected line: {}".format(line)
+
+    print(sum([size for size in sizes.values() if size <= 100000]))
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
