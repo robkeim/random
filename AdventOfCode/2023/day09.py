fea9dfd463594230bf3868aa2b3ae127edf2e30e
Rob Keim <robkeim@gmail.com>
2023-12-09 19:12:43 +0100

    [AdventOfCode] 2023 Add day 9 part 2 solution

--- AdventOfCode/2023/day09.py
+++ AdventOfCode/2023/day09.py
@@ -27,7 +27,31 @@ def get_next_value(line):
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("day09.txt").readlines()]
+    lines = [[int(value) for value in line.split()] for line in lines]
+    print(sum([get_prev_value(line) for line in lines]))
+
+
+def get_prev_value(line):
+    lines = [line]
+
+    # Generate all rows
+    while len(set(lines[-1])) != 1:
+        cur_line = lines[-1]
+        next_line = []
+
+        for i in range(len(cur_line) - 1):
+            next_line.append(cur_line[i + 1] - cur_line[i])
+
+        lines.append(next_line)
+
+    # Calculate next value
+    next_value = lines[-1][0]
+
+    for i in range(len(lines) - 2, -1, -1):
+        next_value = lines[i][0] - next_value
+
+    return next_value
 
 
 def main():
