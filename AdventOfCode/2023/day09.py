ddba650e8b8e1a2fccc5ed1f073a98191dbd922a
Rob Keim <robkeim@gmail.com>
2023-12-09 19:07:14 +0100

    [AdventOfCode] 2023 Add day 9 part 1 solution

---
+++ AdventOfCode/2023/day09.py
@@ -0,0 +1,39 @@
+def part1():
+    lines = [line.strip() for line in open("day09.txt").readlines()]
+    lines = [[int(value) for value in line.split()] for line in lines]
+    print(sum([get_next_value(line) for line in lines]))
+
+
+def get_next_value(line):
+    lines = [line]
+
+    # Generate all rows
+    while len(set(lines[-1])) != 1:
+        cur_line = lines[-1]
+        next_line = []
+
+        for i in range(len(cur_line) - 1):
+            next_line.append(cur_line[i + 1] - cur_line[i])
+
+        lines.append(next_line)
+
+    # Calculate next value
+    next_value = lines[-1][-1]
+
+    for i in range(len(lines) - 2, -1, -1):
+        next_value += lines[i][-1]
+
+    return next_value
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
