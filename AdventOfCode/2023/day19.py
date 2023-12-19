fbaeef751e86d45d547ab612a399290522540f49
Rob Keim <Rob.keim@expensya.com>
2023-12-19 22:09:46 +0100

    [AdventOfCode] 2023 Add day 19 part 2 solution

--- AdventOfCode/2023/day19.py
+++ AdventOfCode/2023/day19.py
@@ -62,7 +62,85 @@ def part1():
 
 
 def part2():
-    pass
+    mappings = open("day19.txt").read().split("\n\n")[0].split()
+    rules = defaultdict(list)
+
+    # Parse mappings
+    for mapping in mappings:
+        match = re.match(r"([a-z]+)\{(.+)\}", mapping)
+        key = match.group(1)
+
+        for rule in match.group(2).split(","):
+            match = re.match(r"([xmas])([<>])(\d+):(([a-z]+)|A|R)", rule)
+
+            if not match:
+                rules[key].append(rule)
+                continue
+
+            rules[key].append([match.group(1), match.group(2), int(match.group(3)), match.group(4)])
+
+    to_process = [("in", [])]
+    successes = []
+
+    while len(to_process) > 0:
+        cur_rule, constraints = to_process.pop()
+
+        if cur_rule == "R":
+            continue
+
+        if cur_rule == "A":
+            successes.append(constraints)
+            continue
+
+        new_constraints = []
+
+        for rule in rules[cur_rule]:
+            if not isinstance(rule, list):
+                to_process.append((rule, list(constraints) + list(new_constraints)))
+            else:
+                item, operator, value, dest = rule
+
+                to_process.append((dest, list(constraints) + list(new_constraints) + [(item, operator, value)]))
+                new_constraints.append(invert_logic(item, operator, value))
+
+    result = 0
+
+    for constraints in successes:
+        mins = {
+            "x": 1,
+            "m": 1,
+            "a": 1,
+            "s": 1
+        }
+
+        maxes = {
+            "x": 4000,
+            "m": 4000,
+            "a": 4000,
+            "s": 4000
+        }
+
+        for item, operator, value in constraints:
+            if operator == ">":
+                mins[item] = max(mins[item], value + 1)
+            else:
+                maxes[item] = min(maxes[item], value - 1)
+
+        product = 1
+
+        for char in "xmas":
+            product *= maxes[char] - mins[char] + 1
+
+        result += product
+
+    print(result)
+
+
+def invert_logic(item, operator, value):
+    if operator == "<":
+        return item, ">", value - 1
+    else:
+        return item, "<", value + 1
 
 
 def main():
