6eec649aef03ee4676f084fe7f683931447e68f3
Rob Keim <robkeim@gmail.com>
2024-12-14 13:10:26 +0100

    [AdventOfCode] 2024 Add day 14 part 2

--- AdventOfCode/2024/day14.py
+++ AdventOfCode/2024/day14.py
@@ -43,19 +43,77 @@ def print_grid(num_r, num_c, robot_positions):
 
         for c in range(num_c):
             if (r, c) in robot_positions:
-                row += str(robot_positions[(r, c)])
+                row += "X"
             else:
                 row += "."
 
         print(row)
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("day14.txt").readlines()]
+    num_r = 103
+    num_c = 101
+    robots = []
+
+    for line in lines:
+        p, v = line.split()
+        p_x, p_y = [int(value) for value in p[2:].split(",")]
+        v_x, v_y = [int(value) for value in v[2:].split(",")]
+
+        robots.append((p_x, p_y, v_x, v_y))
+
+    num_seconds = 0
+
+    while True:
+        if num_seconds % 1000 == 0:
+            print("Num seconds:", num_seconds)
+
+        robot_positions = set()
+        robot_rows = defaultdict(list)
+
+        for p_x, p_y, v_x, v_y in robots:
+            x = (p_x + (v_x + num_c) * num_seconds) % num_c
+            y = (p_y + (v_y + num_r) * num_seconds) % num_r
+
+            robot_positions.add((x, y))
+            robot_rows[x].append(y)
+
+        possible_answer = False
+        for row in robot_rows:
+            if possible_answer:
+                break
+
+            cols = sorted(robot_rows[row])
+
+            if cols == [0, 8, 12, 15, 16, 30, 31, 55, 58, 59, 77, 82, 88]:
+                pass
+
+            max_continuous = 0
+            i = 0
+            j = 0
+
+            while i < len(cols):
+                while j + 1 < len(cols) and cols[j + 1] == cols[j] + 1:
+                    j += 1
+
+                max_continuous = max(max_continuous, j - i)
+                i = j + 1
+                j = i
+
+            max_continuous = max(max_continuous, j - i)
+
+            if max_continuous >= 10:
+                possible_answer = True
+                print("Potential answer after", num_seconds, "seconds")
+                print_grid(num_r, num_c, robot_positions)
+
+        num_seconds += 1
 
 
 def main():
     part1()
-    part2()
+    # Commenting out part 2 because it requires manually looking at the output while the algorithm runs forever
+    # part2()
 
 
 if __name__ == "__main__":
