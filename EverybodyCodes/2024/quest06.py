234cdcfc07a92bd6bfc19f7c4a6611c8b11ee262
Rob Keim <Rob.keim@medius.com>
2024-12-20 16:37:53 +0100

    [EverybodyCodes] Add quest 6 part 3

--- EverybodyCodes/2024/quest06.py
+++ EverybodyCodes/2024/quest06.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-
+from functools import lru_cache
 
 def part1():
     lines = [line.strip() for line in open("quest06_p1.txt").readlines()]
@@ -65,8 +65,51 @@ def part2():
     print(answer)
 
 
+graph = defaultdict(set)
+
 def part3():
-    pass
+    lines = [line.strip() for line in open("quest06_p3.txt").readlines()]
+
+    for line in lines:
+        source, destinations = line.split(":")
+
+        if source == "ANT" or source == "BUG":
+            continue
+
+        for destination in destinations.split(","):
+            if destination == "ANT" or destination == "BUG":
+                continue
+
+            graph[source].add(destination)
+
+    lengths = defaultdict(set)
+
+    for path in compute_paths("RR"):
+        length = len(path.replace("_", ""))
+        lengths[length].add("".join([node[0] for node in path.split("_")]))
+
+    max_length = 0
+    answer = None
+    for length, words in lengths.items():
+        if len(words) == 1 and length > max_length:
+            answer = list(words)[0]
+            max_length = length
+
+    print(answer)
+
+
+@lru_cache(1_000_000)
+def compute_paths(node):
+    if node == "@":
+        return ["@"]
+
+    results = []
+
+    for destination in graph[node]:
+
+        results += [node + "_" + path for path in compute_paths(destination)]
+
+    return results
 
 
 def main():
