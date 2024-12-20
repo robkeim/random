ec101c0b71fd3553ce5a2b12f6c619ae78822b76
Rob Keim <robkeim@gmail.com>
2024-12-20 23:44:33 +0100

    [EverybodyCodes] 2024 Add quest 11 part 2

--- EverybodyCodes/2024/quest11.py
+++ EverybodyCodes/2024/quest11.py
@@ -1,27 +1,34 @@
 from collections import defaultdict
 
-pairings = defaultdict(list)
-
 def part1():
     lines = [line.strip() for line in open("quest11_p1.txt").readlines()]
+    pairings = defaultdict(list)
 
     for line in lines:
         start, end = line.split(":")
         for item in end.split(","):
             pairings[start].append(item)
 
-    print(expand("A", 4))
+    print(expand("A", 4, pairings))
 
 
-def expand(termine, num_days):
+def expand(termine, num_days, pairings):
     if num_days == 0:
         return 1
 
-    return sum([expand(value, num_days - 1) for value in pairings[termine]])
+    return sum([expand(value, num_days - 1, pairings) for value in pairings[termine]])
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("quest11_p2.txt").readlines()]
+    pairings = defaultdict(list)
+
+    for line in lines:
+        start, end = line.split(":")
+        for item in end.split(","):
+            pairings[start].append(item)
+
+    print(expand("Z", 10, pairings))
 
 
 def part3():
