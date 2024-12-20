9e65f442b8f6bf66a146bf4a76eeac2ef6db439d
Rob Keim <robkeim@gmail.com>
2024-12-20 22:41:17 +0100

    [EverbodyCodes] 2024 Add quest 9 part 2

--- EverybodyCodes/2024/quest09.py
+++ EverybodyCodes/2024/quest09.py
@@ -17,7 +17,19 @@ def part1():
     print(sum([num_stamps[ball] for ball in balls]))
 
 def part2():
-    pass
+    balls = [int(line.strip()) for line in open("quest09_p2.txt").readlines()]
+    num_stamps = [sys.maxsize] * (max(balls) + 1)
+    stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30]
+
+    for stamp in stamps:
+        num_stamps[stamp] = 1
+
+    for num in range(2, len(num_stamps)):
+        for stamp in stamps:
+            if stamp <= num:
+                num_stamps[num] = min(num_stamps[num], 1 + num_stamps[num - stamp])
+
+    print(sum([num_stamps[ball] for ball in balls]))
 
 
 def part3():
