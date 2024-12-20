54de5752c0665540f23d57052b41d410ad31ba30
Rob Keim <robkeim@gmail.com>
2024-12-20 23:30:43 +0100

    [EverybodyCodes] 2024 Add quest 10 part 2

--- EverybodyCodes/2024/quest10.py
+++ EverybodyCodes/2024/quest10.py
@@ -12,7 +12,28 @@ def part1():
 
 
 def part2():
-    pass
+    grid = [list(line.strip()) for line in open("quest10_p2.txt").readlines()]
+    power = 0
+    r_offset = 0
+
+    while r_offset < len(grid):
+        c_offset = 0
+
+        while c_offset < len(grid[0]):
+            word = ""
+            for r in range(2, 6):
+                for c in range(2, 6):
+                    row = grid[r_offset + r][c_offset] + grid[r_offset + r][c_offset + 1] + grid[r_offset + r][c_offset + 6] + grid[r_offset + r][c_offset + 7]
+                    col = grid[r_offset][c_offset + c] + grid[r_offset + 1][c_offset + c] + grid[r_offset + 6][c_offset + c] + grid[r_offset + 7][c_offset + c]
+                    word += list(set(row).intersection(set(col)))[0]
+
+            power += sum([(i + 1) * (ord(letter) - ord("A") + 1) for i, letter in enumerate(word)])
+
+            c_offset += 9
+
+        r_offset += 9
+
+    print(power)
 
 
 def part3():
