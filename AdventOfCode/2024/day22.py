c56d242d30084c4c41ea799e38247b51dba4c315
Rob Keim <robkeim@gmail.com>
2024-12-22 21:22:39 +0100

    [AdventOfCode] 2024 Add day 22 part 2

--- AdventOfCode/2024/day22.py
+++ AdventOfCode/2024/day22.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def part1():
     secret_numbers = [int(line.strip()) for line in open("day22.txt").readlines()]
 
@@ -14,7 +17,39 @@ def get_next_secret_number(number, num_iterations):
 
 
 def part2():
-    pass
+    secret_numbers = [int(line.strip()) for line in open("day22.txt").readlines()]
+    num_bananas = defaultdict(int)
+
+    for secret_number in secret_numbers:
+        seen = set()
+        deltas, values = get_secret_sequence(secret_number, 2000)
+
+        for i in range(4, len(deltas) + 1):
+            key = tuple(deltas[i - 4:i])
+
+            if key not in seen:
+                seen.add(key)
+                num_bananas[key] += values[i - 1]
+
+    print(max(num_bananas.values()))
+
+
+def get_secret_sequence(number, num_iterations):
+    deltas = []
+    values = []
+    prev = number % 10
+
+    for _ in range(num_iterations):
+        number = ((number * 64) ^ number) % 16_777_216
+        number = ((number // 32) ^ number) % 16_777_216
+        number = ((number * 2048) ^ number) % 16_777_216
+
+        cur = number % 10
+        deltas.append(cur - prev)
+        values.append(cur)
+        prev = cur
+
+    return deltas, values
 
 
 def main():
