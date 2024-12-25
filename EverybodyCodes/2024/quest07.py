216e3214e1d88e19c2f424de00872d2c93c03a44
Rob Keim <robkeim@gmail.com>
2024-12-25 21:31:20 +0100

    [EverybodyCodes] 2024 Add quest 7 part 2

--- EverybodyCodes/2024/quest07.py
+++ EverybodyCodes/2024/quest07.py
@@ -25,7 +25,74 @@ def part1():
 
 
 def part2():
-    pass
+    track = parse_track()
+
+    lines = [line.strip() for line in open("quest07_p2.txt").readlines()]
+
+    ranking = []
+
+    for line in lines:
+        key, plan = line.split(":")
+        plan = plan.split(",")
+        total_power = 0
+        cur_power = 10
+
+        for i in range(10 * len(track)):
+            track_value = track[i % len(track)]
+
+            if track_value == "+":
+                cur_power += 1
+            elif track_value == "-":
+                cur_power -= 1
+            else:
+                value = plan[i % len(plan)]
+
+                if value == "+":
+                    cur_power += 1
+                elif value == "-":
+                    cur_power -= 1
+
+            total_power += cur_power
+
+        ranking.append((-total_power, key))
+
+    ranking = sorted(ranking)
+    print("".join([value[1] for value in ranking]))
+
+
+raw_track = """
+S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=--
+-                                                                     -
+=                                                                     =
++                                                                     +
+=                                                                     +
++                                                                     =
+=                                                                     =
+-                                                                     -
+--==++++==+=+++-=+=-=+=-+-=+-=+-=+=-=+=--=+++=++=+++==++==--=+=++==+++-
+"""
+
+
+def parse_track():
+    grid = [line.strip() for line in raw_track.strip().split("\n")]
+    num_r = len(grid)
+    num_c = len(grid[0])
+
+    top = ""
+    bottom = ""
+
+    for c in range(1, num_c - 1):
+        top += grid[0][c]
+        bottom += grid[-1][-c - 1]
+
+    left = ""
+    right = ""
+
+    for r in range(num_r):
+        right += grid[r][-1]
+        left += grid[-r - 1][0]
+
+    return top + right + bottom + left
 
 
 def part3():
