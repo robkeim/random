62584791e826b31e4be3d0968f6886cd6c39b28f
Rob Keim <robkeim@gmail.com>
2024-12-25 12:25:27 +0100

    [EverybodyCodes] Add quest 9 part 3

--- EverybodyCodes/2024/quest09.py
+++ EverybodyCodes/2024/quest09.py
@@ -33,7 +33,37 @@ def part2():
 
 
 def part3():
-    pass
+    balls = [int(line.strip()) for line in open("quest09_p3.txt").readlines()]
+    num_stamps = [sys.maxsize] * (max(balls) // 2 + 103)
+    stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101]
+
+    for stamp in stamps:
+        num_stamps[stamp] = 1
+
+    for num in range(2, len(num_stamps)):
+        for stamp in stamps:
+            if stamp <= num:
+                num_stamps[num] = min(num_stamps[num], 1 + num_stamps[num - stamp])
+
+    answer = 0
+
+    for ball in balls:
+        mid = ball // 2
+
+        best = sys.maxsize
+
+        for i in range(-102, 102):
+            one = mid + i
+            two = ball - one
+
+            if abs(one - two) > 100:
+                continue
+
+            best = min(best, num_stamps[one] + num_stamps[two])
+
+        answer += best
+
+    print(answer)
 
 
 def main():
