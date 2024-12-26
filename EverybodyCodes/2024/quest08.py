48f59f70e6e9958c8a9749af2b42ea494253860e
Rob Keim <robkeim@gmail.com>
2024-12-26 22:07:23 +0100

    [EverybodyCodes] Add quest 8 part 2

--- EverybodyCodes/2024/quest08.py
+++ EverybodyCodes/2024/quest08.py
@@ -12,7 +12,21 @@ def part1():
 
 
 def part2():
-    pass
+    num_priests = int(open("quest08_p2.txt").read().strip())
+
+    num_priest_acolytes = 1111
+    num_available_blocks = 20_240_000
+    thickness = 1
+    width = 1
+    total_blocks = 1
+
+    while total_blocks < num_available_blocks:
+        width += 2
+        thickness = (thickness * num_priests) % num_priest_acolytes
+        blocks = width * thickness
+        total_blocks += blocks
+
+    print((total_blocks - num_available_blocks) * width)
 
 
 def part3():
