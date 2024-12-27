8de4e6e8fd913051d076b70d289fb383d4144fab
Rob Keim <robkeim@gmail.com>
2024-12-27 23:24:47 +0100

    [EverybodyCodes] 2024 Add quest 18 part 2

--- EverybodyCodes/2024/quest18.py
+++ EverybodyCodes/2024/quest18.py
@@ -3,6 +3,15 @@ from collections import deque
 
 def part1():
     grid = [line.strip() for line in open("quest18_p1.txt").readlines()]
+    solve_parts_1_and_2(grid)
+
+
+def part2():
+    grid = [line.strip() for line in open("quest18_p2.txt").readlines()]
+    solve_parts_1_and_2(grid)
+
+
+def solve_parts_1_and_2(grid):
     num_r = len(grid)
     num_c = len(grid[0])
     neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -12,7 +21,7 @@ def part1():
 
     for r in range(num_r):
         for c in range(num_c):
-            if c == 0 and grid[r][c] == ".":
+            if (c == 0 or c == num_c - 1) and grid[r][c] == ".":
                 to_process.append((0, r, c))
 
             if grid[r][c] == "P":
@@ -41,10 +50,6 @@ def part1():
 
             if 0 <= next_r < num_r and 0 <= next_c < num_c and (next_r, next_c) not in seen and grid[next_r][next_c] in ".P":
                 to_process.append((num_steps + 1, next_r, next_c))
-
-
-def part2():
-    pass
 
 
 def part3():
