a3cb182376621a1f23de16be6c6ee9bdaa748aa4
Rob Keim <robkeim@gmail.com>
2024-12-28 23:49:01 +0100

    [EverybodyCodes] 2024 Add quest 14 part 3

--- EverybodyCodes/2024/quest14.py
+++ EverybodyCodes/2024/quest14.py
@@ -1,3 +1,7 @@
+import sys
+from collections import deque
+
+
 def part1():
     directions = open("quest14_p1.txt").read().strip()
 
@@ -42,7 +46,71 @@ def part2():
 
 
 def part3():
-    pass
+    plants = [line.strip() for line in open("quest14_p3.txt").readlines()]
+    leaves = set()
+    trunk = set()
+    segments = set()
+
+    dirs = {
+        "R": (0, 1, 0),
+        "D": (-1, 0, 0),
+        "L": (0, -1, 0),
+        "U": (1, 0, 0),
+        "F": (0, 0, 1),
+        "B": (0, 0, -1)
+    }
+
+    for plant in plants:
+        x = y = z = 0
+
+        for direction in plant.split(","):
+            dy, dx, dz = dirs[direction[0]]
+
+            for _ in range(int(direction[1:])):
+                x += dx
+                y += dy
+                z += dz
+
+                if x == 0 and z == 0:
+                    trunk.add(y)
+
+                segments.add((x, y, z))
+
+        leaves.add((x, y, z))
+
+    answer = sys.maxsize
+
+    for trunk_y in trunk:
+        cost = 0
+
+        for start_x, start_y, start_z in leaves:
+            to_process = deque()
+            to_process.append((0, start_x, start_y, start_z))
+            seen = set()
+
+            while to_process:
+                steps, x, y, z = to_process.popleft()
+
+                if x == 0 and y == trunk_y and z == 0:
+                    cost += steps
+                    break
+
+                if (x, y, z) in seen:
+                    continue
+
+                seen.add((x, y, z))
+
+                for dx, dy, dz in dirs.values():
+                    next_x = x + dx
+                    next_y = y + dy
+                    next_z = z + dz
+
+                    if (next_x, next_y, next_z) in segments:
+                        to_process.append((steps + 1, next_x, next_y, next_z))
+
+        answer = min(answer, cost)
+
+    print(answer)
 
 
 def main():
