27ad8d513c17e573fbafc168f13964c9b24b2ea1
Rob Keim <robkeim@gmail.com>
2024-12-28 23:09:50 +0100

    [EverybodyCodes] 2024 Add quest 18 part 3

--- EverybodyCodes/2024/quest18.py
+++ EverybodyCodes/2024/quest18.py
@@ -1,3 +1,4 @@
+import sys
 from collections import deque
 
 
@@ -53,7 +54,44 @@ def solve_parts_1_and_2(grid):
 
 
 def part3():
-    pass
+    grid = [line.strip() for line in open("quest18_p3.txt").readlines()]
+    num_r = len(grid)
+    num_c = len(grid[0])
+    dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    costs = dict()
+
+    for start_r in range(num_r):
+        for start_c in range(num_c):
+            if grid[start_r][start_c] != "P":
+                continue
+
+            seen = set()
+            to_process = deque()
+            to_process.append((0, start_r, start_c))
+
+            while to_process:
+                num_steps, r, c = to_process.popleft()
+
+                if (r, c) in seen:
+                    continue
+
+                seen.add((r, c))
+
+                if grid[r][c] == ".":
+                    if (r, c) not in costs:
+                        costs[(r, c)] = num_steps
+                    else:
+                        costs[(r, c)] += num_steps
+
+                for dr, dc in dirs:
+                    next_r = r + dr
+                    next_c = c + dc
+
+                    if 0 <= next_r < num_r and 0 <= next_c < num_c and (next_r, next_c) not in seen and grid[next_r][next_c] != "#":
+                        to_process.append((num_steps + 1, next_r, next_c))
+
+    print(min(costs.values()))
 
 
 def main():
