9ea250fddebda5445b3e0566f5b484369891ca1e
Rob Keim <robkeim@gmail.com>
2024-12-30 10:51:18 +0100

    [EverybodyCodes] 2024 Add quest 19 part 3

--- EverybodyCodes/2024/quest19.py
+++ EverybodyCodes/2024/quest19.py
@@ -51,7 +51,55 @@ def part2():
 
 
 def part3():
-    pass
+    lines = [list(line.strip()) for line in open("quest19_p3.txt").readlines()]
+    directions = lines[0]
+    grid = lines[2:]
+    num_r = len(grid)
+    num_c = len(grid[0])
+    coordinates_grid = [[(r, c) for c in range(num_c)] for r in range(num_r)]
+    count = 0
+
+    for r in range(1, num_r - 1):
+        for c in range(1, num_c - 1):
+            coordinates_grid = rotate(coordinates_grid, r, c, directions[count % len(directions)])
+            count += 1
+
+    permutation = dict()
+
+    for r in range(num_r):
+        for c in range(num_c):
+            permutation[(r, c)] = coordinates_grid[r][c]
+
+    num_iterations = 1048576000
+    permutation = exp_by_squaring(permutation, num_iterations)
+
+    final_grid = [["." for c in range(num_c)] for r in range(num_r)]
+
+    for start_r, start_c in permutation:
+        dest_r, dest_c = permutation[(start_r, start_c)]
+
+        final_grid[start_r][start_c] = grid[dest_r][dest_c]
+
+    print_grid(final_grid)
+
+
+# https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+def exp_by_squaring(x, n):
+   if n == 1:
+      return x
+   elif n % 2 == 0:
+       return exp_by_squaring(multiply(x, x), n // 2)
+   else:
+        return multiply(x, exp_by_squaring(multiply(x, x), (n - 1) // 2))
+
+
+def multiply(x1, x2):
+    result = dict()
+
+    for key in x1.keys():
+        result[key] = x1[x2[key]]
+
+    return result
 
 
 def main():
