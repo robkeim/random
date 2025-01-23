4f9185dfef871241e8c58e48f3f38dca0ff2513e
Rob Keim <Rob.keim@medius.com>
2025-01-23 01:49:25 +0100

    [AdventOfCode] 2022 Add day 21 part 2

--- AdventOfCode/2022/day21.py
+++ AdventOfCode/2022/day21.py
@@ -35,7 +35,62 @@ def part1():
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("day21.txt").readlines()]
+    values = dict()
+    formulas = dict()
+
+    for line in lines:
+        key, value = line.split(": ")
+        value = value.split()
+
+        if len(value) == 1:
+            values[key] = int(value[0])
+        else:
+            formulas[key] = value
+
+    def yell(key):
+        if key in values:
+            return values[key]
+
+        left, operator, right = formulas[key]
+        left = yell(left)
+        right = yell(right)
+
+        if operator == "+":
+            return left + right
+        elif operator == "-":
+            return left - right
+        elif operator == "*":
+            return left * right
+        elif operator == "/":
+            return left / right
+        else:
+            raise Exception(f"Invalid operator: {operator}")
+
+    key1, _, key2 = formulas["root"]
+    needed = yell(key2) # This part isn't generic because it assumes "humn" is on the left side of the equation
+
+    low = 0
+    high = 1
+    values["humn"] = high
+
+    while yell(key1) > needed:
+        high *= 2
+        values["humn"] = high
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        values["humn"] = mid
+        result = yell(key1)
+
+        if result < needed:
+            high = mid - 1
+        elif result > needed:
+            low = mid + 1
+        else:
+            print(mid)
+            break
 
 
 def main():
