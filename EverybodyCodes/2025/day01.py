c61a991bffa8cf83d028c03bac6a9b64d7d85da9
Rob Keim <robkeim@gmail.com>
2025-11-04 00:04:39 +0100

    [EverybodyCodes 2025] Add quest 1 part 1

---
+++ EverybodyCodes/2025/day01.py
@@ -0,0 +1,32 @@
+def part1():
+    lines = [line.strip() for line in open("quest01_p1.txt").readlines()]
+    names = lines[0].split(",")
+
+    index = 0
+
+    for instruction in lines[2].split(","):
+        value = int(instruction[1:])
+        if instruction[0] == "L":
+            index = max(0, index - value)
+        else:
+            index = min(len(names) - 1, index + value)
+
+    print(names[index])
+
+
+def part2():
+    pass
+
+
+def part3():
+    pass
+
+
+def main():
+    part1()
+    part2()
+    part3()
+
+
+if __name__ == "__main__":
+    main()
---
+++ EverybodyCodes/2025/skeleton.py
@@ -0,0 +1,20 @@
+def part1():
+    lines = [line.strip() for line in open("questXX.txt").readlines()]
+
+
+def part2():
+    pass
+
+
+def part3():
+    pass
+
+
+def main():
+    part1()
+    part2()
+    part3()
+
+
+if __name__ == "__main__":
+    main()
