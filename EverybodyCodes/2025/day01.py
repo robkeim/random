5b79841321a46aacf32044ec3ba9014dee211a65
Rob Keim <robkeim@gmail.com>
2025-11-04 00:07:26 +0100

    [EverybodyCodes 2025] Add quest 1 part 2

--- EverybodyCodes/2025/day01.py
+++ EverybodyCodes/2025/day01.py
@@ -15,7 +15,22 @@ def part1():
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("quest01_p2.txt").readlines()]
+    names = lines[0].split(",")
+
+    index = 0
+
+    for instruction in lines[2].split(","):
+        value = int(instruction[1:])
+        if instruction[0] == "L":
+            index -= value
+        else:
+            index += value
+
+    index %= len(names)
+    index = (index + len(names)) % len(names)
+
+    print(names[index])
 
 
 def part3():
