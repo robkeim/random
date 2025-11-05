c78605fada40b89925b35fb252223cd40e39a093
Rob Keim <robkeim@gmail.com>
2025-11-05 16:56:16 +0100

    [EverybodyCodes 2025] Add quest 2 part 2

--- EverybodyCodes/2025/quest02.py
+++ EverybodyCodes/2025/quest02.py
@@ -1,9 +1,5 @@
-import os
-
-
 def part1():
-    base_dir = os.path.dirname(__file__)
-    raw_input = open(os.path.join(base_dir, "quest02_p1.txt")).read().strip()
+    raw_input = open("quest02_p1.txt").read().strip()
     left, right = raw_input[2:].strip("[]").split(",")
     A = [int(left), int(right)]
 
@@ -16,7 +12,6 @@ def part1():
 
     print(str(result).replace(" ", ""))
 
-    
 
 def add(x, y):
     x1, y1 = x
@@ -33,11 +28,48 @@ def multiply(x, y):
 def divide(x, y):
     x1, y1 = x
     x2, y2 = y
-    return [x1 // x2, y1 // y2]
+    return [x1 // x2 if x1 > 0 else -(-x1 // x2), y1 // y2 if y1 > 0 else -(-y1 // x2)]
 
 
 def part2():
-    pass
+    raw_input = open("quest02_p2.txt").read().strip()
+    left, right = raw_input[2:].strip("[]").split(",")
+    A = [int(left), int(right)]
+    num_engravings = 0
+
+    for start_y in range(101):
+        row = ""
+
+        for start_x in range(101):
+            result = [0, 0]
+            point_under_examination = [A[0] + 10 * start_x, A[1] + 10 * start_y]
+
+            should_engrave = True
+
+            for _ in range(100):
+                result = multiply(result, result)
+                result = divide(result, [100000, 100000])
+                result = add(result, point_under_examination)
+
+                if not valid_range_to_engrave(result):
+                    should_engrave = False
+                    break
+
+            if should_engrave:
+                num_engravings += 1
+                row += "x"
+            else:
+                row += "."
+
+        #print(row)
+
+    print(num_engravings)
+
+
+def valid_range_to_engrave(num):
+    x, y = num
+
+    return -1000000 <= x <= 1000000 and -1000000 <= y <= 1000000
 
 
 def part3():
