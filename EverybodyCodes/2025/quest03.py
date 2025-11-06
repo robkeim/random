847ebd307b7f5559c9f56ad7c27f681fe8e60b56
Rob Keim <robkeim@gmail.com>
2025-11-06 01:39:04 +0100

    [EverybodyCodes 2025] Add quest 3 part 1

---
+++ EverybodyCodes/2025/quest03.py
@@ -0,0 +1,20 @@
+def part1():
+    print(sum(set([int(num) for num in open("quest03_p1.txt").read().strip().split(",")])))
+
+
+def part2():
+    pass
+
+
+def part3():
+    pass
+
+
+def main():
+    part1()
+    part2()
+    part3()
+
+
+if __name__ == "__main__":
+    main()
