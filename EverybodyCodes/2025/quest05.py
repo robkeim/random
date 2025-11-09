81dd0efa037fb502ff53346db8ddf9c7bdb150b8
Rob Keim <robkeim@gmail.com>
2025-11-09 23:57:18 +0100

    [EverybodyCodes 2025] Add quest 5 part 3

--- EverybodyCodes/2025/quest05.py
+++ EverybodyCodes/2025/quest05.py
@@ -31,7 +31,45 @@ def part2():
 
 
 def part3():
-    pass
+    swords = sorted([calculate_sword(line) for line in open("quest05_p3.txt").readlines()], reverse=True)
+    print(sum([(idx + 1) * value[2] for idx, value in enumerate(swords)]))
+
+
+def calculate_sword(line):
+    id_, rest = line.split(":")
+    values = [int(value) for value in rest.split(",")]
+    fishbone = [[None, values[0], None]]
+
+    for value in values[1:]:
+        added = False
+        for row in fishbone:
+            if value < row[1] and row[0] is None:
+                row[0] = value
+                added = True
+            elif value > row[1] and row[2] is None:
+                row[2] = value
+                added = True
+
+            if added:
+                break
+
+        if not added:
+            fishbone.append([None, value, None])
+
+    quality = int("".join([str(row[1]) for row in fishbone]))
+
+    levels = []
+
+    for row in fishbone:
+        value = ""
+
+        for item in row:
+            if item is not None:
+                value += str(item)
+
+        levels.append(int(value))
+
+    return quality, levels, int(id_)
 
 
 def main():
