128a71f2ceaef505cd90d0701f2ae04e72aa4b4b
Rob Keim <robkeim@gmail.com>
2025-11-12 23:45:46 +0100

    [EverybodyCodes 2025] Add quest 7 part 2

--- EverybodyCodes/2025/quest07.py
+++ EverybodyCodes/2025/quest07.py
@@ -24,7 +24,29 @@ def part1():
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("quest07_p2.txt").readlines()]
+    names = lines[0].split(",")
+
+    pairs = set()
+
+    for line in lines[2:]:
+        first, remaining = line.split(" > ")
+        for char in remaining.split(","):
+            pairs.add(first + char)
+
+    result = 0
+
+    for index, name in enumerate(names):
+        found = True
+        for i in range(len(name) - 1):
+            if name[i:i + 2] not in pairs:
+                found = False
+                break
+
+        if found:
+            result += index + 1
+
+    print(result)
 
 
 def part3():
