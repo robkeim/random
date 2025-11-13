daf33d4439267ceb6b8878e733ed7ca6173b8419
Rob Keim <robkeim@gmail.com>
2025-11-13 16:51:02 +0100

    [EverybodyCodes 2025] Add quest 8 part 3

--- EverybodyCodes/2025/quest08.py
+++ EverybodyCodes/2025/quest08.py
@@ -32,7 +32,34 @@ def part2():
 
 
 def part3():
-    pass
+    nails = [int(nail) for nail in open("quest08_p3.txt").read().strip().split(",")]
+
+    threads = []
+
+    for i in range(len(nails) - 1):
+        min_ = min(nails[i], nails[i + 1])
+        max_ = max(nails[i], nails[i + 1])
+        threads.append((min_, max_))
+
+    max_cuts = 0
+
+    for small in range(1, 257):
+        for big in range(small + 1, 257):
+            num_cuts = 0
+
+            for thread_small, thread_big in threads:
+                if small == thread_small and big == thread_big:
+                    num_cuts += 1
+                    continue
+
+                sorted_values = "".join([values[1] for values in sorted([(small, "A"), (big, "A"), (thread_small, "B"), (thread_big, "B")])])
+
+                if (sorted_values == "ABAB" or sorted_values == "BABA") and len({small, big, thread_small, thread_big}) == 4:
+                    num_cuts += 1
+
+            max_cuts = max(max_cuts, num_cuts)
+
+    print(max_cuts)
 
 
 def main():
