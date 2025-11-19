d0912e0f76b537220d16e6f22881644f443bb72b
Rob Keim <robkeim@gmail.com>
2025-11-20 00:29:43 +0100

    [EverybodyCodes 2025] Add quest 13 part 2

--- EverybodyCodes/2025/quest13.py
+++ EverybodyCodes/2025/quest13.py
@@ -15,7 +15,22 @@ def part1():
 
 
 def part2():
-    pass
+    ranges = [line.strip() for line in open("quest13_p2.txt").readlines()]
+    right = []
+    left = []
+
+    for i, num in enumerate(ranges):
+        low, high = [int(value) for value in num.split("-")]
+        segment = [value for value in range(low, high + 1)]
+
+        if i % 2 == 0:
+            right += segment
+        else:
+            left += segment
+
+    clock = [1] + right + left[::-1]
+
+    print(clock[20252025 % len(clock)])
 
 
 def part3():
