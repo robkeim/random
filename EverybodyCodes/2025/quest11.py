1647aa97bdc43f168ad5c8a498f7f67ca6a06d2a
Rob Keim <robkeim@gmail.com>
2025-11-19 23:52:43 +0100

    [EverybodyCodes 2025] Add quest 11 part 1

---
+++ EverybodyCodes/2025/quest11.py
@@ -0,0 +1,59 @@
+def part1():
+    columns = [int(line.strip()) for line in open("quest11_p1.txt").readlines()]
+
+    # Phase one
+    has_moved = True
+    round_number = 0
+
+    while has_moved:
+        has_moved = False
+        round_number += 1
+
+        for i in range(len(columns) - 1):
+            if columns[i] > columns[i + 1]:
+                columns[i] -= 1
+                columns[i + 1] += 1
+                has_moved = True
+
+        if round_number == 11:
+            print(calculate_checksum(columns))
+            return
+
+    # Phase two
+    has_moved = True
+
+    while has_moved:
+        has_moved = False
+        round_number += 1
+
+        for i in range(len(columns) - 1):
+            if columns[i] < columns[i + 1]:
+                columns[i] += 1
+                columns[i + 1] -= 1
+                has_moved = True
+
+        if round_number == 11:
+            print(calculate_checksum(columns))
+            return
+
+
+def calculate_checksum(columns):
+    return sum([(index + 1) * value for index, value in enumerate(columns)])
+
+
+def part2():
+    pass
+
+
+def part3():
+    pass
+
+
+def main():
+    part1()
+    part2()
+    part3()
+
+
+if __name__ == "__main__":
+    main()
