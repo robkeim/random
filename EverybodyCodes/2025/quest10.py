ee04c0c73a7fe3a5173e96116a90e551884f03f1
Rob Keim <robkeim@gmail.com>
2025-11-21 22:57:19 +0100

    [EverybodyCodes 2025] Add quest 10 part 2

--- EverybodyCodes/2025/quest10.py
+++ EverybodyCodes/2025/quest10.py
@@ -36,7 +36,56 @@ def part1():
 
 
 def part2():
-    pass
+    grid = [line.strip() for line in open("quest10_p2.txt").readlines()]
+    num_r = len(grid)
+    num_c = len(grid[0])
+    dragon_positions = None
+    sheep = set()
+    hideouts = set()
+
+    for r in range(num_r):
+        for c in range(num_c):
+            if grid[r][c] == "D":
+                dragon_positions = {(r, c)}
+            elif grid[r][c] == "S":
+                sheep.add((r, c))
+            elif grid[r][c] == "#":
+                hideouts.add((r, c))
+            elif grid[r][c] == ".":
+                pass  # Nothing to do
+            else:
+                assert False, f"Invalid character: {grid[r][c]}"
+
+    sheep_eaten = 0
+
+    for turn in range(20):
+        dragon_positions = get_next_dragon_positions(dragon_positions, num_r, num_c)
+
+        next_sheep = set()
+
+        for r, c in sheep:
+            if ((r + turn, c) in dragon_positions and (r + turn, c) not in hideouts) or ((r + turn + 1, c) in dragon_positions and (r + turn + 1, c) not in hideouts):
+                sheep_eaten += 1
+            else:
+                next_sheep.add((r, c))
+
+        sheep = next_sheep
+
+    print(sheep_eaten)
+
+
+def get_next_dragon_positions(positions, num_r, num_c):
+    next_positions = set()
+
+    for r, c in positions:
+        for dr, dc in [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2)]:
+            next_r = r + dr
+            next_c = c + dc
+
+            if 0 <= next_r < num_r and 0 <= next_c < num_c:
+                next_positions.add((next_r, next_c))
+
+    return next_positions
 
 
 def part3():
