79a978f66234e63c67477d40b3ef678a89ab590d
Rob Keim <robkeim@gmail.com>
2025-11-26 00:11:20 +0100

    [EverybodyCodes 2025] Add quest 17 part 2

--- EverybodyCodes/2025/quest17.py
+++ EverybodyCodes/2025/quest17.py
@@ -22,7 +22,41 @@ def part1():
 
 
 def part2():
-    pass
+    grid = [line.strip() for line in open("quest17_p2.txt").readlines()]
+    num_r = len(grid)
+    num_c = len(grid[0])
+
+    blocks = set()
+
+    for r in range(num_r):
+        for c in range(num_c):
+            if grid[r][c] == "@":
+                Xv, Yv = r, c
+            else:
+                blocks.add((r, c, int(grid[r][c])))
+
+    R = 1
+    max_blocks = 0
+    max_r = 0
+
+    while len(blocks) > 0:
+        num_removed = 0
+        next_blocks = set()
+
+        for Xc, Yc, value in blocks:
+            if (Xv - Xc) * (Xv - Xc) + (Yv - Yc) * (Yv - Yc) <= R * R:
+                num_removed += value
+            else:
+                next_blocks.add((Xc, Yc, value))
+
+        if num_removed > max_blocks:
+            max_blocks = num_removed
+            max_r = R
+
+        blocks = next_blocks
+        R += 1
+
+    print(max_blocks * max_r)
 
 
 def part3():
