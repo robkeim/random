2bd925a9ad4aec396fc9c0c67fdd4bfb5524e454
Rob Keim <robkeim@gmail.com>
2025-11-30 20:57:56 +0100

    [EverybodyCodes 2025] Add quest 20 part 2

--- EverybodyCodes/2025/quest20.py
+++ EverybodyCodes/2025/quest20.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def part1():
     grid = [line.strip() for line in open("quest20_p1.txt").readlines()]
     num_r = len(grid)
@@ -39,7 +42,54 @@ def get_direction(r, c):
 
 
 def part2():
-    pass
+    grid = [line.strip() for line in open("quest20_p2.txt").readlines()]
+    num_r = len(grid)
+    num_c = len(grid[0])
+    t = set()
+
+    for r in range(num_r):
+        for c in range(num_c):
+            if grid[r][c] == "T":
+                t.add((r, c))
+            elif grid[r][c] == "S":
+                start = (r, c)
+                t.add((r, c))
+            elif grid[r][c] == "E":
+                end = (r, c)
+                t.add((r, c))
+
+    to_process = deque([(start[0], start[1], 0)])
+    seen = set()
+
+    while to_process:
+        r, c, num_steps = to_process.popleft()
+
+        if r == end[0] and c == end[1]:
+            print(num_steps)
+            return
+
+        if (r, c) in seen:
+            continue
+
+        seen.add((r, c))
+
+        # Right
+        if (r, c + 1) in t:
+            to_process.append((r, c + 1, num_steps + 1))
+
+        # Left
+        if (r, c - 1) in t:
+            to_process.append((r, c - 1, num_steps + 1))
+
+        # Down
+        if get_direction(r, c) == "U" and (r + 1, c) in t:
+            to_process.append((r + 1, c, num_steps + 1))
+
+        # Up
+        if get_direction(r, c) == "D" and (r - 1, c) in t:
+            to_process.append((r - 1, c, num_steps + 1))
+
+    assert False, "No solution found"
 
 
 def part3():
