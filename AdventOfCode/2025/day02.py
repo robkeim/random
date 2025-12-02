f248c8805e557c3d96575de759a96ec5004cdf26
Rob Keim <robkeim@gmail.com>
2025-12-02 10:49:49 +0100

    [AdventOfCode 2025] Add day 2 part 2

--- AdventOfCode/2025/day02.py
+++ AdventOfCode/2025/day02.py
@@ -22,7 +22,27 @@ def part1():
 
 
 def part2():
-    pass
+    ranges = [line.strip() for line in open("day02.txt").read().split(",")]
+
+    result = 0
+
+    for start_and_end in ranges:
+        low, high = start_and_end.split("-")
+
+        for num in range(int(low), int(high) + 1):
+            num_str = str(num)
+
+            for i in range(1, len(num_str) // 2 + 1):
+                if len(set(chunk(num_str, i))) == 1:
+                    result += num
+                    break
+
+    print(result)
+
+
+def chunk(arr, n):
+    for i in range(0, len(arr), n):
+        yield arr[i:i + n]
 
 
 def main():
