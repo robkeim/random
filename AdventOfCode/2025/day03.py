11cf549a761842bc518aa4766ee281e582cfd872
Rob Keim <robkeim@gmail.com>
2025-12-03 21:44:02 +0100

    [AdventOfCode 2025] Add day 3 part 2

--- AdventOfCode/2025/day03.py
+++ AdventOfCode/2025/day03.py
@@ -16,7 +16,29 @@ def part1():
 
 
 def part2():
-    pass
+    nums = [line.strip() for line in open("day03.txt").readlines()]
+
+    result = 0
+
+    for num_str in nums:
+        num = ""
+        start_index = 0
+
+        for digit in range(12):
+            max_val = num_str[start_index]
+            max_index = start_index
+
+            for i in range(start_index, len(num_str) - (12 - digit - 1)):
+                if num_str[i] > max_val:
+                    max_val = num_str[i]
+                    max_index = i
+
+            num += max_val
+            start_index = max_index + 1
+
+        result += int(num)
+
+    print(result)
 
 
 def main():
