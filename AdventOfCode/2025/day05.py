ffb5a851fc797c26332f6d2702df92141e08a61b
Rob Keim <robkeim@gmail.com>
2025-12-05 13:25:36 +0100

    [AdventOfCode 2025] Add day 5 part 2

--- AdventOfCode/2025/day05.py
+++ AdventOfCode/2025/day05.py
@@ -21,7 +21,38 @@ def part1():
 
 
 def part2():
-    pass
+    lines = [line.strip() for line in open("day05.txt").readlines() if line.strip()]
+
+    ranges = []
+
+    for line in lines:
+        if "-" in line:
+            start, end = line.split("-")
+            ranges.append((int(start), int(end)))
+
+    found_overlap = True
+
+    while found_overlap:
+        found_overlap = False
+        ranges = sorted(ranges)
+
+        for i in range(len(ranges) - 1):
+            left_start, left_end = ranges[i]
+            right_start, right_end = ranges[i + 1]
+
+            if right_start <= left_end:
+                found_overlap = True
+                del ranges[i]
+                del ranges[i]
+                ranges.append((left_start, max(left_end, right_end)))
+                break
+
+    result = 0
+
+    for start, end in ranges:
+        result += end - start + 1
+
+    print(result)
 
 
 def main():
